b1d9154a99d4f8749c637c1aa84a513f748aaf42
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-19 23:35:53 +0900

    feat: create [WebSecurityConfig]

---
+++ src/main/java/myproject/demo/KeyCloak/infrastructure/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package myproject.demo.KeyCloak.infrastructure;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+/*
+인증은 이것만 있으면 됨
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+@KeycloakConfiguration
+// 서버 안에어 security 관련 처리 용도
+public class WebSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/user/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
+    /*
+    토큰 인증은 직접 구현을 하지 않고,
+    이렇게만 하면 알아서 구현
+     */
+    public void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(new KeycloakAuthenticationProvider());
+    }
+
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+
+}
