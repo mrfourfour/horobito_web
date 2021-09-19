1e76b5a4bbc8e3e0e5943b13b9589fcacb91d843
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-19 23:15:26 +0900

    chore : init setting

---
+++ src/main/java/myproject/demo/WebNovelApplication.java
@@ -0,0 +1,13 @@
+package myproject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNovelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebNovelApplication.class, args);
+    }
+
+}
