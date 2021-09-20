c7f97555c2c8357576f1f008d33eb724ce2df017
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-20 16:28:01 +0900

    feat: create <signup>, <save>, <checkDuplicateUser> method in [UserService]

--- src/main/java/myproject/demo/User/service/UserService.java
+++ src/main/java/myproject/demo/User/service/UserService.java
@@ -2,14 +2,20 @@ package myproject.demo.User.service;
 
 
 import lombok.RequiredArgsConstructor;
+import myproject.demo.KeyCloak.service.DuplicateUserSignUpException;
 import myproject.demo.KeyCloak.service.Token;
 import myproject.demo.KeyCloak.service.TokenProvider;
 import myproject.demo.KeyCloak.service.TokenRequest;
+import myproject.demo.User.domain.Password;
+import myproject.demo.User.domain.User;
+import myproject.demo.User.domain.UserRepository;
+import myproject.demo.User.domain.Username;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
 
     private final TokenProvider tokenProvider;
 
@@ -17,5 +23,24 @@ public class UserService {
         TokenRequest tokenRequest
                 = TokenRequest.create(username, password);
         return tokenProvider.issue(tokenRequest);
+    }
+
+    public void signUp(String username, String password) {
+        TokenRequest tokenRequest
+                = TokenRequest.create(username, password);
+        tokenProvider.signUp(tokenRequest);
+        save(username, password);
+    }
+
+    public void save(String username, String password){
+        User user = User.create(Username.create(username), Password.create(password));
+        userRepository.save(user);
+    }
+
+
+    public void checkDuplicateUser(String usernname){
+        if (userRepository.existsByUsername(Username.create(usernname))){
+            throw new DuplicateUserSignUpException();
+        }
     }
 }
