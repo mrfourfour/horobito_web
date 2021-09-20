205d53f87e3a458b8022ba40f4cb16aac0e32b67
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-20 22:01:03 +0900

    feat: add <Authority> class

--- src/main/java/myproject/demo/User/domain/User.java
+++ src/main/java/myproject/demo/User/domain/User.java
@@ -3,11 +3,9 @@ package myproject.demo.User.domain;
 
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 @Entity
@@ -18,23 +16,29 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    @Embedded
     private Username username;
 
+    @Embedded
     private Password password;
 
     private boolean deleted;
 
+    @Embedded
+    private Authority auth;
 
-    private User(Username username, Password password) {
+
+    private User(Username username, Password password, Authority auth) {
         this.username = username;
         this.password = password;
         this.deleted = false;
+        this.auth = auth;
     }
 
-    public static User create(Username username, Password password){
-        return new User(username, password);
+    public static User create(Username username, Password password, Authority auth){
+        return new User(username, password, auth);
     }
+
     public Long getUserId(){
         return this.id;
     }
@@ -46,6 +50,8 @@ public class User {
     public String getPassword(){
         return this.password.getPassword();
     }
+
+
 
     public void delete(){
         this.deleted = true;
