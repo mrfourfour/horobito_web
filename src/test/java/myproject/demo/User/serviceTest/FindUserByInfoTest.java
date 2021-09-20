49c0309358ebcd8eb0dfd7ec6b54ba0b0e0df7d6
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-20 22:11:18 +0900

    test: create Test about [ Find By Username Test :  normal condition ]

---
+++ src/test/java/myproject/demo/User/serviceTest/FindUserByInfoTest.java
@@ -0,0 +1,52 @@
+package myproject.demo.User.serviceTest;
+
+import myproject.demo.KeyCloak.service.DuplicateUserSignUpException;
+import myproject.demo.KeyCloak.service.TokenProvider;
+import myproject.demo.KeyCloak.service.TokenRequest;
+import myproject.demo.User.controller.UserExceptionHandler;
+import myproject.demo.User.domain.*;
+import myproject.demo.User.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class FindUserByInfoTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    TokenProvider tokenProvider;
+
+    @Mock
+    UserExceptionHandler handler;
+
+    @DisplayName(" Find By Username Test 1. normal condition ")
+    @Test
+    public void test1(){
+
+        UserService sut = new UserService(userRepository, tokenProvider);
+
+        Optional<User> user = Optional.of(User.create(
+                Username.create("user"),
+                Password.create("pwd"),
+                Authority.create("auth")
+        ));
+
+        when(userRepository.findByUsername(any())).thenReturn(user);
+
+        assertEquals("user",sut.findUserByUsername(any()).getUsername() );
+
+    }
+}
