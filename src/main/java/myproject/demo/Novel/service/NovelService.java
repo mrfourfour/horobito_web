66764421d01c443a1a49aa50fa55f4325a9b4c16
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-22 12:49:08 +0900

    feat: create [NovelService] class and create <createNovel> method and other methods

---
+++ src/main/java/myproject/demo/Novel/service/NovelService.java
@@ -0,0 +1,46 @@
+package myproject.demo.Novel.service;
+
+
+import lombok.RequiredArgsConstructor;
+import myproject.demo.Novel.domain.*;
+import myproject.demo.User.service.UserDto;
+import myproject.demo.User.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NovelService {
+
+    private final UserService userService;
+    private final NovelRepository novelRepository;
+
+    public NovelDto createNovel(String title, String description){
+        checkDuplicateTitle(title);
+        UserDto userDto = userService.findLoggedUser();
+        Novel novel = Novel.create(
+                Title.create(title),
+                Description.create(description),
+                AuthorId.create(userDto.getUserId())
+        );
+        save(novel);
+        return createNovelDto(novel);
+    }
+
+    private void checkDuplicateTitle(String title) {
+        if (novelRepository.existsByTitle(Title.create(title))){
+            throw new DuplicateNovelException();
+        }
+    }
+
+    private NovelDto createNovelDto(Novel novel) {
+        return new NovelDto(
+          novel.getTitle(),
+          novel.getDescription(),
+        userService.findUserByUserId(novel.getAuthorId()).getUsername()
+        );
+    }
+
+    private void save(Novel novel) {
+        novelRepository.save(novel);
+    }
+}
