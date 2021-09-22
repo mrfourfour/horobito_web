132462d586b60ecb5b673122920cea9fcfd1b025
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-22 20:09:53 +0900

    feat: create <create> method in [CategoryService] class

--- src/main/java/myproject/demo/category/service/CategoryService.java
+++ src/main/java/myproject/demo/category/service/CategoryService.java
@@ -2,6 +2,8 @@ package myproject.demo.category.service;
 
 
 import lombok.RequiredArgsConstructor;
+import myproject.demo.category.domain.Category;
+import myproject.demo.category.domain.CategoryName;
 import myproject.demo.category.domain.CategoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,5 +12,17 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CategoryService {
 
+    private final CategoryRepository categoryRepository;
 
+    @Transactional
+    public void create(String name){
+        checkAlreadyExistence(name);
+        categoryRepository.save(Category.create(CategoryName.create(name)));
+    }
+
+    private void checkAlreadyExistence(String name) {
+        if (categoryRepository.existsByCategoryName(CategoryName.create(name))){
+            throw new IllegalArgumentException();
+        }
+    }
 }
