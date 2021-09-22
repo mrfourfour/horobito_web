535e18012dbf8d4f1525ee73d4684b71ece6c4ab
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-23 00:26:15 +0900

    feat: create <create> method in [CategoryNovelRelationService] class

--- src/main/java/myproject/demo/category_novel/service/CategoryNovelRelationService.java
+++ src/main/java/myproject/demo/category_novel/service/CategoryNovelRelationService.java
@@ -5,17 +5,38 @@ import lombok.RequiredArgsConstructor;
 import myproject.demo.Novel.service.NovelService;
 import myproject.demo.User.service.UserService;
 import myproject.demo.category.service.CategoryService;
+import myproject.demo.category_novel.domain.CategoryNovelRelation;
+import myproject.demo.category_novel.domain.CategoryNovelRelationId;
 import myproject.demo.category_novel.domain.CategoryNovelRelationRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Table;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryNovelRelationService {
-    private final CategoryNovelRelationRepository categoryNovelRelationRepository;
+    private final CategoryNovelRelationRepository relationRepository;
 
     private final UserService userService;
 
     private final NovelService novelService;
 
     private final CategoryService categoryService;
+
+    @Transactional
+    public void create(Long novelId, List<Long> categoryIds) {
+        List<CategoryNovelRelation> relations
+                = categoryIds
+                .stream()
+                .filter(categoryId ->
+                        !relationRepository.existsById(CategoryNovelRelationId.create(categoryId, novelId)))
+                .map(categoryId -> CategoryNovelRelation.create(categoryId, novelId)).collect(Collectors.toList());
+        relationRepository.saveAll(relations);
+    }
+
+
+
 }
