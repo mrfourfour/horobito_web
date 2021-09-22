808c45dd189e2c88731e6e1922eb676651e61387
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-22 20:10:13 +0900

    feat: create <existsByCategoryName> method in [CategoryRepository] class

--- src/main/java/myproject/demo/category/domain/CategoryRepository.java
+++ src/main/java/myproject/demo/category/domain/CategoryRepository.java
@@ -3,4 +3,5 @@ package myproject.demo.category.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    boolean existsByCategoryName(CategoryName categoryName);
 }
