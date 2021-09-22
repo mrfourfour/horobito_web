f7ec4d4ae5197dfd608995645307c0e8e17a5e56
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-22 20:05:10 +0900

    feat: create [CategoryService] class

---
+++ src/main/java/myproject/demo/category/service/CategoryService.java
@@ -0,0 +1,14 @@
+package myproject.demo.category.service;
+
+
+import lombok.RequiredArgsConstructor;
+import myproject.demo.category.domain.CategoryRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+
+}
