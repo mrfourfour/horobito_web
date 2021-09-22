914e08f25f6913820257448be33d82ed64ce120b
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-22 20:04:51 +0900

    feat: create [CategoryRepository] class

---
+++ src/main/java/myproject/demo/category/domain/CategoryRepository.java
@@ -0,0 +1,6 @@
+package myproject.demo.category.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
