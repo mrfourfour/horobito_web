b15afb44994f8ec005f94de8fcffdce7d87a2f4f
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-22 20:39:15 +0900

    feat: create <findAll> method in [CategoryService]class

--- src/main/java/myproject/demo/category/service/CategoryService.java
+++ src/main/java/myproject/demo/category/service/CategoryService.java
@@ -8,6 +8,9 @@ import myproject.demo.category.domain.CategoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
@@ -15,33 +18,46 @@ public class CategoryService {
     private final CategoryRepository categoryRepository;
 
     @Transactional
-    public void create(String name){
+    public void create(String name) {
         checkAlreadyExistenceByName(name);
         categoryRepository.save(Category.create(CategoryName.create(name)));
     }
 
     @Transactional
-    public void delete(Long categoryId){
+    public void delete(Long categoryId) {
         checkExistence(categoryId);
         Category category = categoryRepository.findById(categoryId).get();
         checkAlreadyDeleted(category);
         category.delete();
     }
 
+    public List<CategoryDto> findAll() {
+        return categoryRepository.findAllByDeleted(false).stream()
+                .map(this::getCategoryDto).collect(Collectors.toList());
+    }
+
+    private CategoryDto getCategoryDto(Category category) {
+        return new CategoryDto(
+                category.getId(),
+                category.getName()
+
+        );
+    }
+
     private void checkAlreadyDeleted(Category category) {
-        if (category.checkDeleted()){
+        if (category.checkDeleted()) {
             throw new IllegalArgumentException();
         }
     }
 
     private void checkExistence(Long categoryId) {
-        if (!categoryRepository.existsById(categoryId)){
+        if (!categoryRepository.existsById(categoryId)) {
             throw new IllegalArgumentException();
         }
     }
 
     private void checkAlreadyExistenceByName(String name) {
-        if (categoryRepository.existsByCategoryName(CategoryName.create(name))){
+        if (categoryRepository.existsByCategoryName(CategoryName.create(name))) {
             throw new IllegalArgumentException();
         }
     }
