efa7bc6831aa95cbc29983580224aa8c63307057
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-22 20:07:34 +0900

    fix: disable [LastReadTime] class

--- src/main/java/myproject/demo/Novel/domain/LastReadTime.java
+++ src/main/java/myproject/demo/Novel/domain/LastReadTime.java
@@ -1,25 +1,25 @@
-package myproject.demo.Novel.domain;
-
-
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Embeddable;
-import java.time.LocalDateTime;
-
-@Embeddable
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class LastReadTime {
-
-    LocalDateTime lastReadTime;
-
-    private LastReadTime(LocalDateTime lastReadTime) {
-        this.lastReadTime = lastReadTime;
-    }
-
-    public static LastReadTime create(LocalDateTime lastReadTime){
-        return new LastReadTime(lastReadTime);
-    }
-}
+//package myproject.demo.Novel.domain;
+//
+//
+//import lombok.AccessLevel;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.Embeddable;
+//import java.time.LocalDateTime;
+//
+//@Embeddable
+//@Getter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//public class LastReadTime {
+//
+//    LocalDateTime lastReadTime;
+//
+//    private LastReadTime(LocalDateTime lastReadTime) {
+//        this.lastReadTime = lastReadTime;
+//    }
+//
+//    public static LastReadTime create(LocalDateTime lastReadTime){
+//        return new LastReadTime(lastReadTime);
+//    }
+//}
