a38dabe31083513a876efb16aea1b54a21b1a3db
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-22 13:08:49 +0900

    feat: add <findByTitle> method in [NovelRepository]

--- src/main/java/myproject/demo/Novel/domain/NovelRepository.java
+++ src/main/java/myproject/demo/Novel/domain/NovelRepository.java
@@ -2,6 +2,11 @@ package myproject.demo.Novel.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface NovelRepository extends JpaRepository<Novel, Long> {
     boolean existsByTitle(Title title);
+
+    Optional<Novel> findByTitle(Title title);
+
 }
