1426ffbb14553cb85092c281944018cc603e68cd
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-22 20:38:03 +0900

    feat: create <findAllByDeleted> method in [CategoryRepository] class

--- src/main/java/myproject/demo/category/domain/CategoryRepository.java
+++ src/main/java/myproject/demo/category/domain/CategoryRepository.java
@@ -2,6 +2,10 @@ package myproject.demo.category.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByCategoryName(CategoryName categoryName);
+
+    List<Category> findAllByDeleted(boolean deleted);
 }
