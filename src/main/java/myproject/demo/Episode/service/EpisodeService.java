b1dd3c6531070304ac4e5c9280f7984d69ed5711
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-29 01:18:38 +0900

    feat: create <changeContent> in [EpisodeService] class

--- src/main/java/myproject/demo/Episode/service/EpisodeService.java
+++ src/main/java/myproject/demo/Episode/service/EpisodeService.java
@@ -2,9 +2,7 @@ package myproject.demo.Episode.service;
 
 
 import lombok.RequiredArgsConstructor;
-import myproject.demo.Episode.domain.ContentURL;
-import myproject.demo.Episode.domain.Episode;
-import myproject.demo.Episode.domain.EpisodeRepository;
+import myproject.demo.Episode.domain.*;
 import myproject.demo.Novel.service.NovelService;
 import myproject.demo.User.service.UserService;
 import org.springframework.stereotype.Service;
@@ -19,11 +17,12 @@ public class EpisodeService {
 
     private final EpisodeRepository episodeRepository;
 
-    public void create(Long novelId, String content){
+    public void create(Long novelId, String content, String authorComment ){
         novelService.checkExistenceById(novelId);
         Episode newEpi = Episode.create(
           novelId,
           getEpisodeNum(novelId),
+          AuthorComment.create(authorComment),
           ContentURL.create(enrollContent(content))
         );
         episodeRepository.save(newEpi);
@@ -35,7 +34,20 @@ public class EpisodeService {
         return "";
     }
 
+    private void changeContent(Long novelId, Long episodeNum, String content){
+        novelService.checkExistenceById(novelId);
+        checkExistence(novelId, episodeNum);
+        editContent(episodeNum, content);
+    }
 
+    private void editContent(Long episodeNum, String content) {
+    }
+
+    private void checkExistence(Long novelId, Long episodeNum) {
+        if (!(episodeRepository.existsById(EpisodeId.create(novelId, episodeNum)))){
+            throw new IllegalArgumentException();
+        }
+    }
 
     private Long getEpisodeNum(Long novelId) {
         if (!episodeRepository.existsByNovelIdAndDeleted(novelId, false)){
