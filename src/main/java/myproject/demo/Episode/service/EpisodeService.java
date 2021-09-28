35f0ac0c27b707b68427071df569e82fe125ea98
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-29 01:13:49 +0900

    feat: create <create> method and methods associated with it in [EpisodeService]

--- src/main/java/myproject/demo/Episode/service/EpisodeService.java
+++ src/main/java/myproject/demo/Episode/service/EpisodeService.java
@@ -2,6 +2,8 @@ package myproject.demo.Episode.service;
 
 
 import lombok.RequiredArgsConstructor;
+import myproject.demo.Episode.domain.ContentURL;
+import myproject.demo.Episode.domain.Episode;
 import myproject.demo.Episode.domain.EpisodeRepository;
 import myproject.demo.Novel.service.NovelService;
 import myproject.demo.User.service.UserService;
@@ -16,4 +18,30 @@ public class EpisodeService {
     private final UserService userService;
 
     private final EpisodeRepository episodeRepository;
+
+    public void create(Long novelId, String content){
+        novelService.checkExistenceById(novelId);
+        Episode newEpi = Episode.create(
+          novelId,
+          getEpisodeNum(novelId),
+          ContentURL.create(enrollContent(content))
+        );
+        episodeRepository.save(newEpi);
+
+    }
+
+    private String enrollContent(String content) {
+
+        return "";
+    }
+
+
+
+    private Long getEpisodeNum(Long novelId) {
+        if (!episodeRepository.existsByNovelIdAndDeleted(novelId, false)){
+            return 1L;
+        }else {
+            return episodeRepository.findTopByNovelIdAndDeleted(novelId, false).get().getEpisodeNum()+1;
+        }
+    }
 }
