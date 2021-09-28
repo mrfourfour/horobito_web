a850da646bd3504ca4714331f36813f747a4f3bc
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-28 21:28:51 +0900

    feat: improve <like> method in [PreferenceService] and create methods associated with it

--- src/main/java/myproject/demo/Preference/service/PreferenceService.java
+++ src/main/java/myproject/demo/Preference/service/PreferenceService.java
@@ -6,6 +6,7 @@ import myproject.demo.Novel.service.NovelService;
 import myproject.demo.Preference.domain.PreferencInfo.PreferenceInfo;
 import myproject.demo.Preference.domain.PreferencInfo.PreferenceInfoId;
 import myproject.demo.Preference.domain.PreferencInfo.PreferenceInfoRepository;
+import myproject.demo.Preference.domain.PreferenceCount.PreferenceCountId;
 import myproject.demo.Preference.domain.PreferenceCount.PreferenceCountRepository;
 import myproject.demo.User.service.UserService;
 import org.springframework.stereotype.Service;
@@ -28,14 +29,27 @@ public class PreferenceService {
     @Transactional
     public void like(Long novelId, Long episodeId){
         Long userId = userService.findLoggedUser().getUserId();
-        checkExistence(PreferenceInfoId.create(novelId, userId, episodeId));
-        novelService.checkExistenceById(novelId);
-        episodeService.checkExistenceById(episodeId);
+        checkAlreadyLiked(PreferenceInfoId.create(novelId, userId, episodeId));
+        checkExistenceNovelAndEpisode(novelId, episodeId);
+        createPreference(novelId,userId,episodeId);
+        increasePreferenceCount(novelId, episodeId);
+    }
+
+    private void createPreference(Long novelId, Long userId, Long episodeId) {
         PreferenceInfo preferenceInfo = PreferenceInfo.create(novelId,userId,episodeId);
         preferenceInfoRepository.save(preferenceInfo);
     }
 
-    private void checkExistence(PreferenceInfoId preferenceInfoId) {
+    private void checkExistenceNovelAndEpisode(Long novelId, Long episodeId) {
+        novelService.checkExistenceById(novelId);
+        episodeService.checkExistenceById(episodeId);
+    }
+
+    private void increasePreferenceCount(Long novelId, Long episodeId) {
+        preferenceCountRepository.findById(PreferenceCountId.create(novelId,episodeId)).get().increase();
+    }
+
+    private void checkAlreadyLiked(PreferenceInfoId preferenceInfoId) {
         if (preferenceInfoRepository.existsById(preferenceInfoId)){
             throw new IllegalArgumentException();
         }
