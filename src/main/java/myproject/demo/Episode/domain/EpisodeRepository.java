134921bac6fcebdf01a35417fecbf79d527378ee
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-29 01:11:28 +0900

    feat: add <existsByNovelIdAndDeleted>, <findTopByNovelIdAndDeleted> methods in [EpisodeRepository]

--- src/main/java/myproject/demo/Episode/domain/EpisodeRepository.java
+++ src/main/java/myproject/demo/Episode/domain/EpisodeRepository.java
@@ -2,5 +2,11 @@ package myproject.demo.Episode.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EpisodeRepository extends JpaRepository<Episode, EpisodeId> {
+
+    boolean existsByNovelIdAndDeleted(Long novelId, boolean deleted);
+
+    Optional<Episode> findTopByNovelIdAndDeleted(Long novelId, boolean deleted);
 }
