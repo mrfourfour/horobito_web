95750112ae735657877ae6597292060c2a71ef71
jhkim <62026666+horobito@users.noreply.github.com>
2021-09-28 20:18:18 +0900

    fix: change name

--- src/main/java/myproject/demo/Preference/domain/PreferenceRepository.java
+++
@@ -1,6 +0,0 @@
-package myproject.demo.Preference.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PreferenceRepository extends JpaRepository<Preference, PreferenceId> {
-}
