41f0a91a098e94cbe3c67e51461ed4d37e263ebd
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-04 20:07:36 +0900

    feat: create [UpdateTimeRepository]

---
+++ src/main/java/myproject/demo/updateTime/domain/UpdateTimeRepository.java
@@ -0,0 +1,6 @@
+package myproject.demo.updateTime.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UpdateTimeRepository extends JpaRepository<UpdateTime, Long> {
+}
