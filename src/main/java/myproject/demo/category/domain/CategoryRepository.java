6647dff746876bf81215577ff2da86115e68b297
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-04 16:40:04 +0900

    feat: add methods in [CategoryRepository]

--- src/main/java/myproject/demo/category/domain/CategoryRepository.java
+++ src/main/java/myproject/demo/category/domain/CategoryRepository.java
@@ -2,10 +2,16 @@ package myproject.demo.category.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByCategoryName(CategoryName categoryName);
 
     List<Category> findAllByDeleted(boolean deleted);
+
+    Optional<Category> findByCategoryName_CategoryNameAndDeleted(String categoryNames, boolean deleted);
+
+    Optional<Category> findByIdAndDeleted(Long id, boolean deleted);
 }
