0a05a5545fbf6f572afe2b50bc2e147cb6795f72
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-04 17:33:51 +0900

    feat: add new method in [PreferenceCountRepository]

--- src/main/java/myproject/demo/Preference/domain/PreferenceCount/PreferenceCountRepository.java
+++ src/main/java/myproject/demo/Preference/domain/PreferenceCount/PreferenceCountRepository.java
@@ -2,5 +2,9 @@ package myproject.demo.Preference.domain.PreferenceCount;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PreferenceCountRepository extends JpaRepository<PreferenceCount, PreferenceCountId> {
+
+    List<PreferenceCount> findAllByNovelId(Long novelId);
 }
