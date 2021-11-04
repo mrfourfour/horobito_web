6f00caa512c58ef366d3f2034aafdf97a4e15249
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-04 17:02:30 +0900

    feat: add fields in [NovelDto] class

--- src/main/java/myproject/demo/Novel/service/NovelDto.java
+++ src/main/java/myproject/demo/Novel/service/NovelDto.java
@@ -4,7 +4,11 @@ import lombok.Value;
 
 @Value
 public class NovelDto {
+    Long novelId;
+    Long authorId;
     String title;
     String description;
     String authorName;
+    String coverImageUrl;
+    boolean deleted;
 }
