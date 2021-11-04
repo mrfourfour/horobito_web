0fd65ce7c543c9ad6ec5b048ba60d7ec01c59e89
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-04 20:07:28 +0900

    feat: create [UpdateTimeDto]

---
+++ src/main/java/myproject/demo/updateTime/service/UpdateTimeDto.java
@@ -0,0 +1,10 @@
+package myproject.demo.updateTime.service;
+
+import lombok.Value;
+import java.time.LocalDateTime;
+
+@Value
+public class UpdateTimeDto{
+    Long id;
+    LocalDateTime updateTime;
+}
