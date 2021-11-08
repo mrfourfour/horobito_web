084b296cb034124df41192bbadbefc4739cf6e8a
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-08 19:05:44 +0900

    test: create Test <Create test 1, Normal Condition>

--- src/test/java/myproject/demo/Novel/serviceTest/CreateTest.java
+++ src/test/java/myproject/demo/Novel/serviceTest/CreateTest.java
@@ -1,14 +1,29 @@
 package myproject.demo.Novel.serviceTest;
 
 
+import myproject.demo.Novel.NovelHelper;
+import myproject.demo.Novel.domain.Novel;
 import myproject.demo.Novel.domain.NovelRepository;
+import myproject.demo.Novel.domain.Title;
+import myproject.demo.Novel.service.NovelDto;
 import myproject.demo.Novel.service.NovelService;
+import myproject.demo.User.domain.*;
+import myproject.demo.User.service.UserDto;
 import myproject.demo.User.service.UserService;
+import org.bouncycastle.util.Strings;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 
 @ExtendWith(MockitoExtension.class)
 public class CreateTest {
@@ -23,6 +38,25 @@ public class CreateTest {
     @Test
     public void test1(){
         NovelService sut = new NovelService(userService, novelRepository);
+
+        UserDto userDto = new UserDto(1L, "authorName");
+        when(userService.findLoggedUser()).thenReturn(userDto);
+        User user = User.create(
+                Username.create("username"),
+                Password.create("password"),
+                Authority.create("auth"),
+                LocalDateTime.now(),
+                Gender.valueOf(Strings.toUpperCase("male"))
+                );
+        when(userService.findUserByUserId(any())).thenReturn(userDto);
+        Novel novel = NovelHelper.create(
+                1L, 1L, "title", "descprition,"
+        ,12, "url");
+
+        when(novelRepository.saveAndFlush(any())).thenReturn(novel);
+
+        sut.createNovel("title", "description", 12, "url");
+        verify(novelRepository, times(1)).saveAndFlush(any());
 
 
     }
