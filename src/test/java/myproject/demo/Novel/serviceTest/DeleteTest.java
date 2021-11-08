eb97d6d54da459a8d0a049ac0f8ba3bcac296126
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-08 22:49:46 +0900

    test : create [Delete Test] : <"Delete Test 2. Abnormal Condition : Already Deleted">

--- src/test/java/myproject/demo/Novel/serviceTest/DeleteTest.java
+++ src/test/java/myproject/demo/Novel/serviceTest/DeleteTest.java
@@ -18,6 +18,7 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -57,6 +58,27 @@ public class DeleteTest {
         assertEquals(false, novel.isDeleted());
         sut.delete(1L);
         assertEquals(true, novel.isDeleted());
+
+
+    }
+
+    @DisplayName("Delete Test 2. Abnormal Condition : Already Deleted")
+    @Test
+    public void test2() throws IllegalAccessException {
+        Novel novel = NovelHelper.create(
+                1L, 1L, "title", "descprition,"
+                ,12, "url");
+        novel.delete();
+
+        NovelService sut = new NovelService(userService, novelRepository);
+        UserDto userDto = new UserDto(1L, "authorName");
+
+        when(novelRepository.existsById(any())).thenReturn(true);
+        when(novelRepository.findById(any())).thenReturn(Optional.of(novel));
+        when(userService.findLoggedUser()).thenReturn(userDto);
+
+
+        assertThrows(IllegalArgumentException.class, ()->sut.delete(1L));
 
 
     }
