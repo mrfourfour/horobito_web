6f92b634825025092f3c2b0525a58e7f769f6888
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-13 20:53:15 +0900

    test: create [Create test] : <("Create Test 2. Abnormal Condition : Duplicate Title">

--- src/test/java/myproject/demo/Category/CreateTest.java
+++ src/test/java/myproject/demo/Category/CreateTest.java
@@ -7,13 +7,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.Arrays;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -32,7 +28,22 @@ public class CreateTest {
         List<String> names = Arrays.asList("one", "two", "three");
 
         sut.createAll(names);
-        verify(categoryRepository, times(3)).saveAndFlush(any());
+        verify(categoryRepository, times(3 )).saveAndFlush(any());
+
+    }
+
+    @DisplayName("Create Test 2. Abnormal Condition : Duplicate Title")
+    @Test
+    public void test2(){
+        CategoryService sut = new CategoryService(categoryRepository);
+
+        List<String> names = Arrays.asList("one", "two", "three");
+
+        when(categoryRepository.existsByCategoryName(any())).thenReturn(true);
+        when(categoryRepository.existsByCategoryName(any())).thenReturn(true);
+        when(categoryRepository.existsByCategoryName(any())).thenReturn(true);
+        sut.createAll(names);
+        verify(categoryRepository, times(0 )).saveAndFlush(any());
 
     }
 
