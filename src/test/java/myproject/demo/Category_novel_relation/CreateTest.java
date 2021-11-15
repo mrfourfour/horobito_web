4b717d34c65227ceffe8a89ab96989df29d28acd
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-15 17:28:38 +0900

    test: create [Category_Novel_Relation test] : <Create test 2. Abnormal Condition : novel is already deleted>

--- src/test/java/myproject/demo/Category_novel_relation/CreateTest.java
+++ src/test/java/myproject/demo/Category_novel_relation/CreateTest.java
@@ -1,6 +1,9 @@
 package myproject.demo.Category_novel_relation;
 
 
+import myproject.demo.Novel.NovelHelper;
+import myproject.demo.Novel.domain.Novel;
+import myproject.demo.Novel.domain.NovelRepository;
 import myproject.demo.Novel.service.NovelService;
 import myproject.demo.User.service.UserService;
 import myproject.demo.category.service.CategoryDto;
@@ -14,10 +17,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,7 +37,12 @@ public class CreateTest {
     CategoryService categoryService;
 
     @Mock
+    NovelRepository novelRepository;
+
+
+    @Mock
     CategoryNovelRelationRepository relationRepository;
+
 
     @DisplayName("Create test 1. Normal Condition")
     @Test
@@ -41,7 +50,7 @@ public class CreateTest {
         CategoryNovelRelationService sut = new CategoryNovelRelationService(
                 relationRepository,
                 userService,
-                novelService,
+                new NovelService(userService, novelRepository),
                 categoryService
         );
 
@@ -52,6 +61,12 @@ public class CreateTest {
         dtoList.add(dto1);
         dtoList.add(dto2);
         dtoList.add(dto3);
+
+        Novel novel = NovelHelper.create(
+                1L, 1L, "title", "descprition,"
+                ,12, "url");
+        when(novelRepository.existsById(any())).thenReturn(true);
+        when(novelRepository.findById(any())).thenReturn(Optional.of(novel));
 
         sut.create(1L, dtoList);
         verify(relationRepository, times(3)).existsById(any());
@@ -65,7 +80,7 @@ public class CreateTest {
         CategoryNovelRelationService sut = new CategoryNovelRelationService(
                 relationRepository,
                 userService,
-                novelService,
+                new NovelService(userService, novelRepository),
                 categoryService
         );
 
@@ -77,6 +92,12 @@ public class CreateTest {
         dtoList.add(dto2);
         dtoList.add(dto3);
 
+        Novel novel = NovelHelper.create(
+                1L, 1L, "title", "descprition,"
+                ,12, "url");
+        when(novelRepository.existsById(any())).thenReturn(true);
+        when(novelRepository.findById(any())).thenReturn(Optional.of(novel));
+
         when(relationRepository.existsById(
                 CategoryNovelRelationId.create(2L, 1L)
         )).thenReturn(true);
@@ -85,6 +106,39 @@ public class CreateTest {
         sut.create(1L, dtoList);
         verify(relationRepository, times(3)).existsById(any());
         verify(relationRepository, times(1)).saveAllAndFlush(any());
+
+    }
+
+    @DisplayName("Create test 2. Abnormal Condition : novel is already deleted")
+    @Test
+    public void test3(){
+
+        CategoryNovelRelationService sut = new CategoryNovelRelationService(
+                relationRepository,
+                userService,
+                new NovelService(userService, novelRepository),
+                categoryService
+        );
+
+        CategoryDto dto1 = new CategoryDto(2L, "c2");
+        CategoryDto dto2 = new CategoryDto(3L, "c3");
+        CategoryDto dto3 = new CategoryDto(4L, "c4");
+        List<CategoryDto> dtoList = new LinkedList<>();
+        dtoList.add(dto1);
+        dtoList.add(dto2);
+        dtoList.add(dto3);
+
+        Novel novel = NovelHelper.create(
+                1L, 1L, "title", "descprition,"
+                ,12, "url");
+        novel.delete();
+
+        when(novelRepository.existsById(any())).thenReturn(true);
+        when(novelRepository.findById(any())).thenReturn(Optional.of(novel));
+
+        assertThrows(IllegalArgumentException.class, ()-> sut.create(1L, dtoList));
+
+
 
     }
 }
