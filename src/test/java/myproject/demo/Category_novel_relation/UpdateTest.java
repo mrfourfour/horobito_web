599aa4fd15cf048b3edaa89d1e0aeea54c245c65
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-16 00:37:09 +0900

    test: create [Category_Novel_Relation test] : <"Update test 2. abnormal Condition : Category or Novel doesn't exist or is already deleted">

--- src/test/java/myproject/demo/Category_novel_relation/UpdateTest.java
+++ src/test/java/myproject/demo/Category_novel_relation/UpdateTest.java
@@ -1,17 +1,13 @@
 package myproject.demo.Category_novel_relation;
 
-
 import myproject.demo.Novel.domain.NovelRepository;
 import myproject.demo.Novel.service.NovelService;
 import myproject.demo.User.service.UserService;
 import myproject.demo.category.domain.CategoryRepository;
 import myproject.demo.category.service.CategoryDto;
 import myproject.demo.category.service.CategoryService;
-import myproject.demo.category_novel.domain.CategoryNovelRelation;
-import myproject.demo.category_novel.domain.CategoryNovelRelationId;
 import myproject.demo.category_novel.domain.CategoryNovelRelationRepository;
 import myproject.demo.category_novel.service.CategoryNovelRelationService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,15 +15,9 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import java.util.LinkedList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -55,7 +45,8 @@ public class UpdateTest {
                 relationRepository,
                 userService,
                 new NovelService(userService, novelRepository),
-                new CategoryService(categoryRepository)
+                new CategoryService(categoryRepository),
+                categoryRepository
         );
 
         CategoryDto dto1 = new CategoryDto(2L, "c2");
@@ -68,6 +59,48 @@ public class UpdateTest {
 
 
         sut.update(9L, dtoList);
+
+
+    }
+
+    @DisplayName("Update test 2. abnormal Condition : Category or Novel doesn't exist or is already deleted ")
+    @Test
+    public void test2() {
+
+
+        CategoryNovelRelationService sut
+                = new CategoryNovelRelationService(
+                relationRepository,
+                userService,
+                new NovelService(userService, novelRepository),
+                new CategoryService(categoryRepository),
+                categoryRepository
+        );
+
+        CategoryDto dto1 = new CategoryDto(0L, "c2");
+        List<CategoryDto> dtoList1 = new LinkedList<>();
+        dtoList1.add(dto1);
+
+        CategoryDto dto2 = new CategoryDto(3L, "c2");
+        List<CategoryDto> dtoList2 = new LinkedList<>();
+        dtoList2.add(dto2);
+
+        CategoryDto dto3 = new CategoryDto(3L, "c2");
+        List<CategoryDto> dtoList3 = new LinkedList<>();
+        dtoList3.add(dto3);
+
+        //category: x, novel : o
+        assertThrows(IllegalArgumentException.class, ()->sut.update(9L, dtoList1));
+
+        //category: deleted, novel : o
+        assertThrows(IllegalArgumentException.class, ()->sut.update(5L, dtoList2));
+
+
+
+        //category: o, novel : x
+        assertThrows(IllegalArgumentException.class, ()->sut.update(0L, dtoList2));
+        //category: o, novel : deleted
+        assertThrows(IllegalArgumentException.class, ()->sut.update(4L, dtoList3));
 
 
     }
