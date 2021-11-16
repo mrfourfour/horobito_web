128723dab851202c5c5f7e98bcd6ff90ccfd3256
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-16 23:25:38 +0900

    test: create [Episode test] : <Create test 1. Normal Condition>

---
+++ src/test/java/myproject/demo/Episode/CreateTest.java
@@ -0,0 +1,75 @@
+package myproject.demo.Episode;
+
+
+import myproject.demo.Episode.domain.Episode;
+import myproject.demo.Episode.domain.EpisodeRepository;
+import myproject.demo.Episode.service.EpisodeService;
+import myproject.demo.Novel.domain.NovelRepository;
+import myproject.demo.Novel.service.NovelService;
+import myproject.demo.User.service.UserDto;
+import myproject.demo.User.service.UserService;
+import myproject.demo.category.domain.CategoryRepository;
+import myproject.demo.category_novel.domain.CategoryNovelRelationRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class CreateTest {
+
+    @Autowired
+    CategoryNovelRelationRepository relationRepository;
+
+    @Autowired
+    NovelRepository novelRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    EpisodeRepository episodeRepository;
+
+    @Mock
+    UserService userService;
+
+    @DisplayName("Create test 1. Normal Condition")
+    @Test
+    public void test1() {
+        EpisodeService sut
+                = new EpisodeService(
+                new NovelService(userService, novelRepository),
+                userService,
+                episodeRepository
+                );
+
+        UserDto userDto = new UserDto(1L, "user1");
+        when(userService.findLoggedUser()).thenReturn(userDto);
+        when(userService.findUserByUserId(any())).thenReturn(userDto);
+
+        Long testNovelId = 1L;
+
+        sut.create(testNovelId,"episdoe content", "1");
+        sut.create(testNovelId, "episode content2", null);
+        sut.create(testNovelId, "episode content3", "3");
+        sut.create(testNovelId, "episode content4", "");
+        sut.create(testNovelId, "episode content5", "5");
+
+        List<Episode> list = episodeRepository.findAllByNovelId(testNovelId);
+
+        assertEquals(5, list.size());
+
+    }
+}
