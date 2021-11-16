569e71ee2e16a14356bb61ce2856a7341f0428cc
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-17 05:15:23 +0900

    fix: fix

--- src/test/java/myproject/demo/UpdateTime/CreateTest.java
+++ src/test/java/myproject/demo/UpdateTime/CreateTest.java
@@ -1,6 +1,8 @@
 package myproject.demo.UpdateTime;
 
 
+import myproject.demo.Novel.service.NovelService;
+import myproject.demo.User.service.UserService;
 import myproject.demo.updateTime.domain.UpdateTime;
 import myproject.demo.updateTime.domain.UpdateTimeRepository;
 import myproject.demo.updateTime.service.UpdateTimeService;
@@ -20,10 +22,18 @@ public class CreateTest {
     @Mock
     UpdateTimeRepository updateTimeRepository;
 
+
+    @Mock
+     NovelService novelService;
+
+    @Mock
+    UserService userService;
+
     @DisplayName("Create test 1. Normal Condition")
     @Test
     public void test1(){
-        UpdateTimeService sut = new UpdateTimeService(updateTimeRepository);
+        UpdateTimeService sut = new UpdateTimeService(
+                updateTimeRepository, novelService, userService);
 
 
         when(updateTimeRepository.saveAndFlush(any())).thenReturn(UpdateTime.create(1L));
