3f09dab041d12788883b9ed55b33bcc4a45795d9
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-17 05:09:48 +0900

    test: create [UpdateTime test] : <Create test 1. Normal Condition >

---
+++ src/test/java/myproject/demo/UpdateTime/CreateTest.java
@@ -0,0 +1,34 @@
+package myproject.demo.UpdateTime;
+
+
+import myproject.demo.updateTime.domain.UpdateTime;
+import myproject.demo.updateTime.domain.UpdateTimeRepository;
+import myproject.demo.updateTime.service.UpdateTimeService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CreateTest {
+
+
+    @Mock
+    UpdateTimeRepository updateTimeRepository;
+
+    @DisplayName("Create test 1. Normal Condition")
+    @Test
+    public void test1(){
+        UpdateTimeService sut = new UpdateTimeService(updateTimeRepository);
+
+
+        when(updateTimeRepository.saveAndFlush(any())).thenReturn(UpdateTime.create(1L));
+        sut.create(1L);
+
+        verify(updateTimeRepository, times(1)).saveAndFlush(any());
+    }
+}
