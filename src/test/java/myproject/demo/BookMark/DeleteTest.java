1ca017769e554bbc9cc6ca00290fb242b32320f7
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-18 04:55:10 +0900

    test: create [BookMark test] : <Delete test 2. abnormal Condition: novel doesn't exist or already deleted>

--- src/test/java/myproject/demo/BookMark/DeleteTest.java
+++ src/test/java/myproject/demo/BookMark/DeleteTest.java
@@ -8,6 +8,7 @@ import myproject.demo.bookmark.domain.BookMark;
 import myproject.demo.bookmark.domain.BookMarkId;
 import myproject.demo.bookmark.domain.BookMarkRepository;
 import myproject.demo.bookmark.service.BookMarkService;
+import myproject.demo.grade.service.GradeService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,12 +16,12 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 
@@ -62,4 +63,31 @@ public class DeleteTest {
         assertTrue(sutObject.get().isDeleted());
 
     }
+
+    @DisplayName("Delete test 2. abnormal Condition: novel doesn't exist or already deleted")
+    @Test
+    public void test3() {
+        BookMarkService sut = new BookMarkService(
+                userService, new NovelService(userService, novelRepository), bookMarkRepository);
+
+        UserDto userDto = new UserDto(1L, "user1");
+
+        //요청한 유저
+        when(userService.findLoggedUser()).thenReturn(userDto);
+        // 소설 작가
+        when(userService.findUserByUserId(any())).thenReturn(userDto);
+
+        Long nonExistNovelId = -1L;
+        Long deletedNovelId = 3L;
+
+        // novel : x
+        assertThrows(IllegalArgumentException.class, ()->sut.delete(nonExistNovelId));
+
+        // novel : deleted
+        assertThrows(IllegalArgumentException.class, ()->sut.delete(deletedNovelId));
+
+
+    }
+
+
 }
