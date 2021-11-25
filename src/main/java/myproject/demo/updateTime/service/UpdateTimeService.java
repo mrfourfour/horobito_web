bd244e50f98aff84bb784ca9878d78d848f45214
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-25 23:26:08 +0900

    feat: add methods and fields in [UpdateTimeService]

--- src/main/java/myproject/demo/updateTime/service/UpdateTimeService.java
+++ src/main/java/myproject/demo/updateTime/service/UpdateTimeService.java
@@ -1,10 +1,15 @@
 package myproject.demo.updateTime.service;
 
 import lombok.RequiredArgsConstructor;
+import myproject.demo.Episode.domain.EpisodeId;
+import myproject.demo.Novel.service.NovelService;
+import myproject.demo.User.service.UserService;
 import myproject.demo.updateTime.domain.UpdateTime;
 import myproject.demo.updateTime.domain.UpdateTimeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,9 +17,47 @@ public class UpdateTimeService {
 
     private final UpdateTimeRepository updateTimeRepository;
 
+    private final NovelService novelService;
+
+    private final UserService userService;
+
     @Transactional
     public UpdateTimeDto create(Long novelId){
+        novelService.checkExistenceById(novelId);
+        novelService.checkRequesterIdentity(
+                userService.findLoggedUser().getUserId(),
+                novelService.getNovelDto(novelId).getAuthorId());
+
         return getDto(updateTimeRepository.saveAndFlush(UpdateTime.create(novelId)));
+    }
+
+    @Transactional
+    public void delete(Long novelId) {
+        novelService.checkExistenceById(novelId);
+        novelService.checkRequesterIdentity(
+                userService.findLoggedUser().getUserId(),
+                novelService.getNovelDto(novelId).getAuthorId());
+
+        updateTimeRepository.findById(novelId).ifPresent(
+                selectedUpdate->{
+                    checkAlreadyDeleted(selectedUpdate);
+                    selectedUpdate.delete();
+                });
+
+    }
+
+    @Transactional
+    public UpdateTimeDto update(Long novelId){
+        novelService.checkExistenceById(novelId);
+        novelService.checkRequesterIdentity(
+                userService.findLoggedUser().getUserId(),
+                novelService.getNovelDto(novelId).getAuthorId());
+
+        Optional<UpdateTime> selected = updateTimeRepository.findById(novelId);
+        selected.ifPresent(selectedUpdateTime->{
+            checkAlreadyDeleted(selectedUpdateTime);
+            selectedUpdateTime.update(); });
+        return getDto(selected.get());
     }
 
     private UpdateTimeDto getDto(UpdateTime updateTime) {
@@ -29,7 +72,9 @@ public class UpdateTimeService {
         return getDto(updateTime);
     }
 
-    private void update(Long novelId){
-        updateTimeRepository.findById(novelId).get().update();
+    private void checkAlreadyDeleted(UpdateTime selectedUpdateTime) {
+        if (selectedUpdateTime.isDeleted()){
+            throw new IllegalArgumentException();
+        }
     }
 }
