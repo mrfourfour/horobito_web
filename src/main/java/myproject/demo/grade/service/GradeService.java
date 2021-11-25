5d5171475190823341e823242e3b0b788c503d0e
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-25 23:14:08 +0900

    feat: add and change some businessLogic in methods in [GradeService]

--- src/main/java/myproject/demo/grade/service/GradeService.java
+++ src/main/java/myproject/demo/grade/service/GradeService.java
@@ -3,9 +3,9 @@ package myproject.demo.grade.service;
 
 import lombok.RequiredArgsConstructor;
 import myproject.demo.Novel.service.NovelService;
+import myproject.demo.User.service.UserService;
 import myproject.demo.grade.domain.Grade;
 import myproject.demo.grade.domain.GradeRepository;
-import myproject.demo.grade.domain.NovelId;
 import myproject.demo.grade.domain.Premium;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,41 +19,67 @@ public class GradeService {
 
     private final GradeRepository gradeRepository;
     private final NovelService novelService;
+    private final UserService userService;
 
     @Transactional
     public GradeDto create(Long novelId, boolean premium){
         novelService.checkExistenceById(novelId);
+        novelService.checkRequesterIdentity(
+                userService.findLoggedUser().getUserId(),
+                novelService.getNovelDto(novelId).getAuthorId());
+
         return getGradeDto(gradeRepository.saveAndFlush(Grade.create(novelId, Premium.create(premium))));
-    }
-
-    private GradeDto getGradeDto(Grade grade) {
-        return new GradeDto(grade.getNovelId(), grade.getPremium());
-    }
-
-    private GradeDto getGradeDto(Long novelId, boolean premium) {
-        return new GradeDto(novelId, premium);
     }
 
     @Transactional
     public void delete(Long novelId){
         novelService.checkExistenceById(novelId);
+        novelService.checkRequesterIdentity(
+                userService.findLoggedUser().getUserId(),
+                novelService.getNovelDto(novelId).getAuthorId());
+
         checkExistence(novelId);
         gradeRepository.findById(novelId).get().delete();
     }
 
     @Transactional
-    public void resurrect(Long novelId){
-        novelService.checkExistenceById(novelId);
-        checkExistence(novelId);
+    public void resurrect(Long id){
+        novelService.checkExistenceById(id);
+        novelService.checkRequesterIdentity(
+                userService.findLoggedUser().getUserId(),
+                novelService.getNovelDto(id).getAuthorId());
+
+        checkJustExistence(id);
+        gradeRepository.findById(id).ifPresent(selectedGrade->{
+                    checkNonAlive(selectedGrade);
+                    selectedGrade.resurrect(); });
+    }
+
+    private void checkNonAlive(Grade selectedGrade) {
+        if (!selectedGrade.getDeleted()){
+            throw new IllegalArgumentException();
+        }
     }
 
     @Transactional
     public GradeDto changePremium(Long novelId, boolean premium){
         novelService.checkExistenceById(novelId);
+        novelService.checkRequesterIdentity(
+                userService.findLoggedUser().getUserId(),
+                novelService.getNovelDto(novelId).getAuthorId());
+
         checkExistence(novelId);
-        checkPremium(novelId, premium);
+//        checkPremium(novelId, premium);
         gradeRepository.findById(novelId).get().changePremium(premium);
         return getGradeDto(novelId, premium);
+    }
+
+    private GradeDto getGradeDto(Grade grade) {
+        return new GradeDto(grade.getId(), grade.getPremium());
+    }
+
+    private GradeDto getGradeDto(Long novelId, boolean premium) {
+        return new GradeDto(novelId, premium);
     }
 
     private void checkPremium(Long novelId, boolean premium) {
@@ -62,8 +88,14 @@ public class GradeService {
         }
     }
 
+    private void checkJustExistence(Long novelId) {
+        if (gradeRepository.findById(novelId).isEmpty()){
+            throw new IllegalArgumentException();
+        }
+    }
+
     private void checkExistence(Long novelId) {
-        if (!gradeRepository.findById(novelId).isPresent()
+        if (gradeRepository.findById(novelId).isEmpty()
                 || gradeRepository.findById(novelId).get().getDeleted()){
             throw new IllegalArgumentException();
         }
@@ -72,14 +104,14 @@ public class GradeService {
     public List<Long> getNovelIdsByGrade(boolean premium){
         return gradeRepository
                 .findAllByPremium_PremiumAndDeleted(premium, false)
-                .stream().map(Grade::getNovelId).collect(Collectors.toList());
+                .stream().map(Grade::getId).collect(Collectors.toList());
 
     }
 
     public List<Long> getAllNovelIds(){
         return gradeRepository
                 .findAllByDeleted(false)
-                .stream().map(Grade::getNovelId).collect(Collectors.toList());
+                .stream().map(Grade::getId).collect(Collectors.toList());
 
     }
 
