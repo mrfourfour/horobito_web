7df62be18493008852fabd5547a6257e6c7fb26b
jhkim <62026666+horobito@users.noreply.github.com>
2021-11-25 23:05:11 +0900

    refactor: delete and change return type of methods in [EpisodeRepository]

--- src/main/java/myproject/demo/Episode/domain/EpisodeRepository.java
+++ src/main/java/myproject/demo/Episode/domain/EpisodeRepository.java
@@ -3,13 +3,13 @@ package myproject.demo.Episode.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface EpisodeRepository extends JpaRepository<Episode, EpisodeId> {
 
     boolean existsByNovelIdAndDeleted(Long novelId, boolean deleted);
 
-    Optional<Episode> findTopByNovelIdAndDeleted(Long novelId, boolean deleted);
 
-    List<Episode> findAllByNovelIdAndDeleted(Long novelId, boolean deleted);
+    List<Episode> findAllByNovelId(Long novelId);
+
+    List<Episode> findAllByNovelIdAndDeleted(Long novelId, boolean b);
 }
