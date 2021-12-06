f4a8c1eccab5eecd187243cb2761ad8de7df2295
jhkim <62026666+horobito@users.noreply.github.com>
2021-12-06 23:37:07 +0900

    refactor: refactoring

--- src/main/java/myproject/demo/manager/viewManager/service/RankingViewManagerService.java
+++ src/main/java/myproject/demo/manager/viewManager/service/RankingViewManagerService.java
@@ -2,7 +2,11 @@ package myproject.demo.manager.viewManager.service;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import myproject.demo.Novel.domain.Age;
+import myproject.demo.Novel.domain.Novel;
+import myproject.demo.Novel.domain.NovelRepository;
 import myproject.demo.Novel.service.NovelService;
+import myproject.demo.Preference.domain.PreferencInfo.PreferenceInfo;
 import myproject.demo.Preference.domain.PreferencInfo.PreferenceInfoRepository;
 import myproject.demo.User.service.UserService;
 import myproject.demo.category_novel.service.CategoryNovelRelationService;
@@ -24,61 +28,100 @@ public class RankingViewManagerService {
     private final NovelManagerService novelManagerService;
     private final NovelService novelService;
     private final UserService userService;
+    private final NovelRepository novelRepository;
 
-    public List<NovelInfoDto> getTopTwentyAllByCategory(
+    public List<NovelInfoDto> getTopAllByCategory(
             String categoryName, String date, Long cursor, int size) {
         LocalDateTime startDay = LocalDate.parse(date).atStartOfDay().plusHours(9);
 
         if (categoryName.equals("all")) {
             HashMap<Long, PreferenceInfoBetweenPeriod> idAndPreference = new HashMap<>();
-            // List 만들고,
-            // 해당 List 를 분할하는데,
-            // 이때 사용하는 정보 : cursor[시작 기준], Page[크기]
-            // cursor : List에서 해당 cursor에 해당하는 id를 찾고, 그 이상부터 탐색
-            // 조건 : List가 정렬되어 있어야 한다.
-
             preferenceInfoRepository.findAllByDeletedAndPreferenceTimeGreaterThanEqual(false, startDay)
                     .forEach(
                             selected -> {
                                 if (novelService.isPresentAndNonDeleted(selected.getNovelId())
                                         && userService.isPresentAndNonDeleted(selected.getUserId())) {
-                                    if (idAndPreference.containsKey(selected.getNovelId())) {
-                                        idAndPreference.get(selected.getNovelId()).increase();
-                                    } else {
-                                        idAndPreference.put(
-                                                selected.getNovelId(), new PreferenceInfoBetweenPeriod(selected.getNovelId(), 1L));
-                                    }
+                                    getMapOfIdAndPreference( idAndPreference, selected);
                                 }
+                            });
+            List<PreferenceInfoBetweenPeriod> info = new ArrayList<>(idAndPreference.values());
+            return getNovelInfoDtosByCursorAndSize(cursor, size, info);
+        } else {
+            // 해당 카테고리를 가지고 있는 novel들의 ids
+            List<Long> novelIds = relationService.getNovelIdsCorrespondingThisCategory(categoryName);
+            return checkPreferenceCountPerNovel(cursor, size, startDay, novelIds);
+        }
+    }
 
+
+    public List<NovelInfoDto> getTopAdultByCategory(String categoryName, String date, Long cursor, int size) {
+        LocalDateTime startDay = LocalDate.parse(date).atStartOfDay().plusHours(9);
+        List<Long> adultNovelIds = novelRepository.findAllByDeletedAndAgeGreaterThanEqual(false, Age.create(18)).stream().map(Novel::getId).collect(Collectors.toList());
+        return getNovelsPreferenceInSuchCategory(categoryName, cursor, size, startDay, adultNovelIds);
+    }
+
+
+    public List<NovelInfoDto> getTopNonAdultByCategory(String categoryName, String date, Long cursor, int size) {
+        LocalDateTime startDay = LocalDate.parse(date).atStartOfDay().plusHours(9);
+        List<Long> nonAdultNovelIds = novelRepository.findAllByDeletedAndAgeLessThan(false, Age.create(18)).stream().map(Novel::getId).collect(Collectors.toList());
+        return getNovelsPreferenceInSuchCategory(categoryName, cursor, size, startDay, nonAdultNovelIds);
+    }
+
+
+
+
+
+
+    private List<NovelInfoDto> getNovelsPreferenceInSuchCategory(String categoryName, Long cursor, int size, LocalDateTime startDay, List<Long> nonAdultNovelIds) {
+        if (categoryName.equals("all")) {
+            HashMap<Long, PreferenceInfoBetweenPeriod> idAndPreference = new HashMap<>();
+            preferenceInfoRepository.findAllByNovelIdInAndDeletedAndPreferenceTimeGreaterThanEqual(nonAdultNovelIds, false, startDay)
+                    .forEach(
+                            selected -> {
+                                if (novelService.isPresentAndNonDeleted(selected.getNovelId())
+                                        && userService.isPresentAndNonDeleted(selected.getUserId())) {
+                                    getMapOfIdAndPreference(idAndPreference, selected);
+                                }
                             });
             List<PreferenceInfoBetweenPeriod> info = new ArrayList<>(idAndPreference.values());
             return getNovelInfoDtosByCursorAndSize(cursor, size, info);
 
         } else {
-            // 해당 카테고리를 가지고 있는 novel들의 ids
-            List<Long> novelIds = relationService.getNovelIdsCorrespondingThisCategory(categoryName);
+            List<Long> filtered = relationService.getNovelIdsCorrespondingThisCategoryOnlyAdult(categoryName, nonAdultNovelIds);
+            return checkPreferenceCountPerNovel(cursor, size, startDay, filtered);
+        }
+    }
 
-            HashSet<Long> checkList = new HashSet<>();
-            // 해당 id 의 novel 별 해당 기간의 좋아요 수
-            List<PreferenceInfoBetweenPeriod> pib = new ArrayList<>();
-            novelIds.forEach(
-                    selectedId -> {
-                        if (novelService.isPresentAndNonDeleted(selectedId)
-                                && userService.isPresentAndNonDeleted(novelService.getNovelDto(selectedId).getAuthorId())) {
-                            if (!checkList.contains(selectedId)) {
-                                checkList.add(selectedId);
-                                int preferenceSize = preferenceInfoRepository.findByNovelIdAndDeletedAndPreferenceTimeGreaterThanEqual(
-                                        selectedId, false, startDay).size();
-                                if (preferenceSize != 0) {
-                                    pib.add(new PreferenceInfoBetweenPeriod(
-                                            selectedId,
-                                            (long) preferenceSize));
-                                }
+    private void getMapOfIdAndPreference(HashMap<Long, PreferenceInfoBetweenPeriod> idAndPreference, PreferenceInfo selected) {
+        if (idAndPreference.containsKey(selected.getNovelId())) {
+            idAndPreference.get(selected.getNovelId()).increase();
+        } else {
+            idAndPreference.put(
+                    selected.getNovelId(), new PreferenceInfoBetweenPeriod(selected.getNovelId(), 1L));
+        }
+    }
+
+
+    private List<NovelInfoDto> checkPreferenceCountPerNovel(Long cursor, int size, LocalDateTime startDay, List<Long> novelIds) {
+        List<PreferenceInfoBetweenPeriod> pib = new ArrayList<>();
+        HashSet<Long> checkList = new HashSet<>();
+        novelIds.forEach(
+                selectedId -> {
+                    if (novelService.isPresentAndNonDeleted(selectedId)
+                            && userService.isPresentAndNonDeleted(novelService.getNovelDto(selectedId).getAuthorId())) {
+                        if (!checkList.contains(selectedId)) {
+                            checkList.add(selectedId);
+                            int preferenceSize = preferenceInfoRepository.findByNovelIdAndDeletedAndPreferenceTimeGreaterThanEqual(
+                                    selectedId, false, startDay).size();
+                            if (preferenceSize != 0) {
+                                pib.add(new PreferenceInfoBetweenPeriod(
+                                        selectedId,
+                                        (long) preferenceSize));
                             }
                         }
-                    });
-            return getNovelInfoDtosByCursorAndSize(cursor, size, pib);
-        }
+                    }
+                });
+        return getNovelInfoDtosByCursorAndSize(cursor, size, pib);
     }
 
     private List<NovelInfoDto> getNovelInfoDtosByCursorAndSize(Long cursor, int size, List<PreferenceInfoBetweenPeriod> pib) {
@@ -104,49 +147,48 @@ public class RankingViewManagerService {
         }
     }
 
+}
+@Getter
+class PreferenceInfoBetweenPeriod implements Comparable<PreferenceInfoBetweenPeriod> {
+    Long novelId;
+    Long preferenceCount;
 
-    @Getter
-    class PreferenceInfoBetweenPeriod implements Comparable<PreferenceInfoBetweenPeriod> {
-        Long novelId;
-        Long preferenceCount;
+    public PreferenceInfoBetweenPeriod(Long novelId, Long preferenceCount) {
+        this.novelId = novelId;
+        this.preferenceCount = preferenceCount;
+    }
 
-        public PreferenceInfoBetweenPeriod(Long novelId, Long preferenceCount) {
-            this.novelId = novelId;
-            this.preferenceCount = preferenceCount;
-        }
+    public void increase() {
+        this.preferenceCount++;
+    }
 
-        public void increase() {
-            this.preferenceCount++;
-        }
+    @Override
+    public int compareTo(PreferenceInfoBetweenPeriod o) {
+        return (int) (this.preferenceCount - o.preferenceCount);
+    }
 
-        @Override
-        public int compareTo(PreferenceInfoBetweenPeriod o) {
-            return (int) (this.preferenceCount - o.preferenceCount);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == null) {
-                return false;
-            }
-            if (this == obj) {
-                System.out.println("Object Same");
-                return true;
-            }
-            if (!(obj instanceof PreferenceInfoBetweenPeriod)) {
-                return false;
-            }
-            PreferenceInfoBetweenPeriod that = (PreferenceInfoBetweenPeriod) obj;
-            if (this.novelId.equals(that.novelId)) {
-                return true;
-            }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
             return false;
         }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(novelId);
+        if (this == obj) {
+            System.out.println("Object Same");
+            return true;
         }
+        if (!(obj instanceof PreferenceInfoBetweenPeriod)) {
+            return false;
+        }
+        PreferenceInfoBetweenPeriod that = (PreferenceInfoBetweenPeriod) obj;
+        if (this.novelId.equals(that.novelId)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(novelId);
     }
 }
 
