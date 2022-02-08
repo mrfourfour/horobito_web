d03df9d558258ab556e17fa25e468ffe23ddae2f
horobito <odkfu1@gmail.com>
2022-02-08 20:02:06 +0900

    feat : apply [NovelViewModelService] in [PreferenceManagerService]

--- src/main/java/myproject/demo/manager/preferenceManager/service/PreferenceManagerService.java
+++ src/main/java/myproject/demo/manager/preferenceManager/service/PreferenceManagerService.java
@@ -3,6 +3,7 @@ package myproject.demo.manager.preferenceManager.service;
 
 import lombok.RequiredArgsConstructor;
 import myproject.demo.Preference.service.PreferenceService;
+import myproject.demo.novelViewModel.service.NovelViewModelService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,14 +13,17 @@ public class PreferenceManagerService {
 
 
     private final PreferenceService preferenceService;
+    private final NovelViewModelService novelViewModelService;
 
     @Transactional
     public void preferEpisode(Long novelId, int episodeId) {
         preferenceService.prefer(novelId, episodeId);
+        novelViewModelService.prefer(novelId);
     }
 
     @Transactional
     public void cancelPreference(Long novelId, int episodeId) {
         preferenceService.cancel(novelId, episodeId);
+        novelViewModelService.cancelPreference(novelId);
     }
 }
