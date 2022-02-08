c17907e468c5a637847888f017fc090cde309897
horobito <odkfu1@gmail.com>
2022-02-08 19:59:30 +0900

    refactor : apply [NovelViewManager] in methods

--- src/main/java/myproject/demo/manager/novelManager/service/NovelManagerService.java
+++ src/main/java/myproject/demo/manager/novelManager/service/NovelManagerService.java
@@ -13,39 +13,28 @@ import myproject.demo.category.service.CategoryService;
 import myproject.demo.category_novel.service.CategoryNovelRelationService;
 import myproject.demo.grade.service.GradeDto;
 import myproject.demo.grade.service.GradeService;
+import myproject.demo.novelViewModel.service.NovelViewModelDto;
+import myproject.demo.novelViewModel.service.NovelViewModelService;
 import myproject.demo.updateTime.service.UpdateTimeDto;
 import myproject.demo.updateTime.service.UpdateTimeService;
-import myproject.demo.view.service.ViewService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
 
 @Service
 @RequiredArgsConstructor
 public class NovelManagerService {
 
     private final NovelService novelService;
-
     private final CategoryService categoryService;
-
     private final CategoryNovelRelationService categoryNovelRelationService;
-
     private final GradeService gradeService;
-
-    private final EpisodeService episodeService;
-
     private final PreferenceService preferenceService;
-
-
     private final UpdateTimeService updateTimeService;
-
+    private final NovelViewModelService novelViewModelService;
     private final BookMarkService bookMarkService;
-
-    private final ViewService viewCountService;
 
 
     @Transactional
@@ -59,6 +48,21 @@ public class NovelManagerService {
         categoryNovelRelationService.create(novelDto.getNovelId(), categoryDtos);
         preferenceService.createTotalCount(novelDto.getNovelId());
 
+        novelViewModelService.create(
+                novelDto.getNovelId(),
+                novelDto.getTitle(),
+                novelDto.getAuthorName(),
+                novelDto.getCoverImageUrl(),
+                novelDto.getDescription(),
+                novelDto.isDeleted(),
+                novelDto.getAge(),
+                gradeDto.isPremium(),
+                updateTimeDto.getUpdateTime(),
+                0L,
+                0,
+                0L,
+                0);
+
         return getNovelInfoDto(
                 novelDto.getNovelId(),
                 novelDto.getTitle(),
@@ -74,71 +78,40 @@ public class NovelManagerService {
                 0L,
                 0,
                 categoryDtos
-
         );
     }
 
     @Transactional
     public NovelInfoDto updateNovelInfo(Long novelId, String title, String description,
-                                        List<String> categories, int age, boolean premium, String coverImageUrl) throws IllegalAccessException {
+                                        List<String> categories, int age, boolean premium, String coverImageUrl){
         categoryService.createAll(categories);
-        NovelDto novelDto = novelService.editNovel(novelId, title, description, age, coverImageUrl);
         List<CategoryDto> categoryDtos = categoryService.findAllByCategoryNames(categories);
         categoryNovelRelationService.update(novelId, categoryDtos);
         UpdateTimeDto updateTimeDto = updateTimeService.getUpdateTime(novelId);
         gradeService.changePremium(novelId, premium);
-        List<Integer> episodeIds = episodeService.findAllByNovelId(novelId)
-                .stream().map(EpisodeDto::getEpisodeId).collect(Collectors.toList());
+
+        NovelViewModelDto novelViewModelDto = novelViewModelService.getViewModel(novelId);
+        novelViewModelService.updateNovelDomainArea(novelId, title, novelViewModelDto.getAuthorName(),
+                coverImageUrl,description, false, age);
+        novelViewModelService.updatePremium(novelId, premium);
+        novelViewModelService.updateTime(novelId, updateTimeDto.getUpdateTime());
 
 
         return getNovelInfoDto(
-                novelDto.getNovelId(),
-                novelDto.getTitle(),
-                novelDto.getAuthorName(),
-                novelDto.getCoverImageUrl(),
-                novelDto.getDescription(),
-                novelDto.isDeleted(),
-                novelDto.getAge(),
-                premium,
-                updateTimeDto.getUpdateTime(),
-                preferenceService.getTotalPreferenceCountByNovelId(novelId).getCount(),
-                bookMarkService.getTotalBookMarkCount(novelId),
-                viewCountService.getTotalViewCount(novelId, episodeIds),
-                episodeService.getTotalEpisode(novelId),
+                novelViewModelDto.getNovelId(),
+                novelViewModelDto.getTitle(),
+                novelViewModelDto.getAuthorName(),
+                novelViewModelDto.getCoverImageUrl(),
+                novelViewModelDto.getDescription(),
+                novelViewModelDto.isDeleted(),
+                novelViewModelDto.getAge(),
+                novelViewModelDto.isPremium(),
+                novelViewModelDto.getUpdateTime(),
+                novelViewModelDto.getPreferenceCount(),
+                novelViewModelDto.getBookMarkCount(),
+                novelViewModelDto.getView(),
+                novelViewModelDto.getEpisodeNumber(),
                 categoryDtos
-        );
-    }
-
-
-    @Transactional
-    public NovelInfoDto keepNovelPrivate(Long novelId) throws IllegalAccessException {
-        novelService.delete(novelId);
-
-        List<CategoryDto> categoryDtos = categoryService.findAllByCategoryIds(categoryNovelRelationService.findCategoryIdsByNovelId(novelId));
-        UpdateTimeDto updateTimeDto = updateTimeService.getUpdateTime(novelId);
-        List<Integer> episodeIds = episodeService.findAllByNovelId(novelId)
-                .stream().map(EpisodeDto::getEpisodeId).collect(Collectors.toList());
-        categoryNovelRelationService.update(novelId, categoryDtos);
-        GradeDto gradeDto = gradeService.getGrade(novelId);
-        NovelDto novelDto = novelService.getNovelDto(novelId);
-
-        return getNovelInfoDto(
-                novelDto.getNovelId(),
-                novelDto.getTitle(),
-                novelDto.getAuthorName(),
-                novelDto.getCoverImageUrl(),
-                novelDto.getDescription(),
-                novelDto.isDeleted(),
-                novelDto.getAge(),
-                gradeDto.isPremium(),
-                updateTimeDto.getUpdateTime(),
-                preferenceService.getTotalPreferenceCountByNovelId(novelId).getCount(),
-                bookMarkService.getTotalBookMarkCount(novelId),
-                viewCountService.getTotalViewCount(novelId, episodeIds),
-                episodeService.getTotalEpisode(novelId),
-                categoryDtos
-
-
         );
     }
 
@@ -148,31 +121,26 @@ public class NovelManagerService {
         preferenceService.deleteTotalPreference(novelId);
         novelService.delete(novelId);
 
-        List<CategoryDto> categoryDtos = categoryService.findAllByCategoryIds(categoryNovelRelationService.findCategoryIdsByNovelId(novelId));
-        UpdateTimeDto updateTimeDto = updateTimeService.getUpdateTime(novelId);
-        List<Integer> episodeIds = episodeService.findAllByNovelId(novelId)
-                .stream().map(EpisodeDto::getEpisodeId).collect(Collectors.toList());
+        List<CategoryDto> categoryDtos = getCategoryDtoList(novelId);
         categoryNovelRelationService.update(novelId, categoryDtos);
-        GradeDto gradeDto = gradeService.getGrade(novelId);
-        NovelDto novelDto = novelService.getNovelDto(novelId);
+        novelViewModelService.delete(novelId);
+        NovelViewModelDto novelViewModelDto = novelViewModelService.getViewModel(novelId);
 
         return getNovelInfoDto(
-                novelDto.getNovelId(),
-                novelDto.getTitle(),
-                novelDto.getAuthorName(),
-                novelDto.getCoverImageUrl(),
-                novelDto.getDescription(),
-                novelDto.isDeleted(),
-                novelDto.getAge(),
-                gradeDto.isPremium(),
-                updateTimeDto.getUpdateTime(),
-                preferenceService.getTotalPreferenceCountByNovelId(novelId).getCount(),
-                bookMarkService.getTotalBookMarkCount(novelId),
-                viewCountService.getTotalViewCount(novelId, episodeIds),
-                episodeService.getTotalEpisode(novelId),
+                novelViewModelDto.getNovelId(),
+                novelViewModelDto.getTitle(),
+                novelViewModelDto.getAuthorName(),
+                novelViewModelDto.getCoverImageUrl(),
+                novelViewModelDto.getDescription(),
+                novelViewModelDto.isDeleted(),
+                novelViewModelDto.getAge(),
+                novelViewModelDto.isPremium(),
+                novelViewModelDto.getUpdateTime(),
+                novelViewModelDto.getPreferenceCount(),
+                novelViewModelDto.getBookMarkCount(),
+                novelViewModelDto.getView(),
+                novelViewModelDto.getEpisodeNumber(),
                 categoryDtos
-
-
         );
     }
 
@@ -180,55 +148,124 @@ public class NovelManagerService {
     public NovelInfoDto resurrect(Long novelId) throws IllegalAccessException {
         novelService.resurrect(novelId);
         preferenceService.resurrectTotalCount(novelId);
-
-        List<CategoryDto> categoryDtos = categoryService.findAllByCategoryIds(categoryNovelRelationService.findCategoryIdsByNovelId(novelId));
-        UpdateTimeDto updateTimeDto = updateTimeService.getUpdateTime(novelId);
-        List<Integer> episodeIds = episodeService.findAllByNovelId(novelId)
-                .stream().map(EpisodeDto::getEpisodeId).collect(Collectors.toList());
+        List<CategoryDto> categoryDtos = getCategoryDtoList(novelId);
         categoryNovelRelationService.update(novelId, categoryDtos);
-        GradeDto gradeDto = gradeService.getGrade(novelId);
-        NovelDto novelDto = novelService.getNovelDto(novelId);
+        novelViewModelService.resurrect(novelId);
+
+        NovelViewModelDto novelViewModelDto = novelViewModelService.getViewModel(novelId);
+
 
         return getNovelInfoDto(
-                novelDto.getNovelId(),
-                novelDto.getTitle(),
-                novelDto.getAuthorName(),
-                novelDto.getCoverImageUrl(),
-                novelDto.getDescription(),
-                novelDto.isDeleted(),
-                novelDto.getAge(),
-                gradeDto.isPremium(),
-                updateTimeDto.getUpdateTime(),
-                preferenceService.getTotalPreferenceCountByNovelId(novelId).getCount(),
-                bookMarkService.getTotalBookMarkCount(novelId),
-                viewCountService.getTotalViewCount(novelId, episodeIds),
-                episodeService.getTotalEpisode(novelId),
+                novelViewModelDto.getNovelId(),
+                novelViewModelDto.getTitle(),
+                novelViewModelDto.getAuthorName(),
+                novelViewModelDto.getCoverImageUrl(),
+                novelViewModelDto.getDescription(),
+                novelViewModelDto.isDeleted(),
+                novelViewModelDto.getAge(),
+                novelViewModelDto.isPremium(),
+                novelViewModelDto.getUpdateTime(),
+                novelViewModelDto.getPreferenceCount(),
+                novelViewModelDto.getBookMarkCount(),
+                novelViewModelDto.getView(),
+                novelViewModelDto.getEpisodeNumber(),
                 categoryDtos
 
 
         );
     }
 
-    public NovelInfoDto viewNovel(Long novelId) {
-        NovelDto novelDto = novelService.getNovelDto(novelId);
-        List<Integer> episodeIds = episodeService.findAllByNovelId(novelId)
-                .stream().map(EpisodeDto::getEpisodeId).collect(Collectors.toList());
-        UpdateTimeDto updateTimeDto = updateTimeService.getUpdateTime(novelId);
-        List<CategoryDto> categoryDtos = categoryService.findAllByCategoryIds(categoryNovelRelationService.findCategoryIdsByNovelId(novelId));
+    @Transactional
+    public NovelInfoDto setBookmark(Long novelId) {
+        bookMarkService.create(novelId);
+        novelViewModelService.increaseBookmarkCount(novelId);
+        NovelViewModelDto novelViewModelDto = novelViewModelService.getViewModel(novelId);
+        List<CategoryDto> categoryDtos = getCategoryDtoList(novelId);
+
         return getNovelInfoDto(
-                novelDto.getNovelId(),
-                novelDto.getTitle(),
-                novelDto.getAuthorName(),
-                novelDto.getCoverImageUrl(),
-                novelDto.getDescription(),
-                novelDto.isDeleted(),
-                novelDto.getAge(),
-                gradeService.getGrade(novelId).isPremium(),
-                updateTimeDto.getUpdateTime(),
-                preferenceService.getTotalPreferenceCountByNovelId(novelId).getCount(),
-                bookMarkService.getTotalBookMarkCount(novelId),
-                viewCountService.getTotalViewCount(novelId, episodeIds),
-                episodeService.getTotalEpisode(novelId),
+                novelViewModelDto.getNovelId(),
+                novelViewModelDto.getTitle(),
+                novelViewModelDto.getAuthorName(),
+                novelViewModelDto.getCoverImageUrl(),
+                novelViewModelDto.getDescription(),
+                novelViewModelDto.isDeleted(),
+                novelViewModelDto.getAge(),
+                novelViewModelDto.isPremium(),
+                novelViewModelDto.getUpdateTime(),
+                novelViewModelDto.getPreferenceCount(),
+                novelViewModelDto.getBookMarkCount(),
+                novelViewModelDto.getView(),
+                novelViewModelDto.getEpisodeNumber(),
+                categoryDtos
+        );
+    }
+
+    @Transactional
+    public NovelInfoDto cancelBookmark(Long novelId){
+        bookMarkService.delete(novelId);
+        novelViewModelService.decreaseBookmarkCount(novelId);
+        List<CategoryDto> categoryDtos = getCategoryDtoList(novelId);
+        NovelViewModelDto novelViewModelDto = novelViewModelService.getViewModel(novelId);
+
+        return getNovelInfoDto(
+                novelViewModelDto.getNovelId(),
+                novelViewModelDto.getTitle(),
+                novelViewModelDto.getAuthorName(),
+                novelViewModelDto.getCoverImageUrl(),
+                novelViewModelDto.getDescription(),
+                novelViewModelDto.isDeleted(),
+                novelViewModelDto.getAge(),
+                novelViewModelDto.isPremium(),
+                novelViewModelDto.getUpdateTime(),
+                novelViewModelDto.getPreferenceCount(),
+                novelViewModelDto.getBookMarkCount(),
+                novelViewModelDto.getView(),
+                novelViewModelDto.getEpisodeNumber(),
+                categoryDtos
+        );
+    }
+
+    public NovelInfoDto viewNovel(Long novelId) {
+        List<CategoryDto> categoryDtos = getCategoryDtoList(novelId);
+        NovelViewModelDto novelViewModel = novelViewModelService.getViewModel(novelId);
+        return getNovelInfoDto(
+                novelViewModel.getNovelId(),
+                novelViewModel.getTitle(),
+                novelViewModel.getAuthorName(),
+                novelViewModel.getCoverImageUrl(),
+                novelViewModel.getDescription(),
+                novelViewModel.isDeleted(),
+                novelViewModel.getAge(),
+                novelViewModel.isPremium(),
+                novelViewModel.getUpdateTime(),
+                novelViewModel.getPreferenceCount(),
+                novelViewModel.getBookMarkCount(),
+                novelViewModel.getView(),
+                novelViewModel.getEpisodeNumber(),
+                categoryDtos
+        );
+    }
+
+    private List<CategoryDto> getCategoryDtoList(Long novelId) {
+        return categoryService.findAllByCategoryIds(categoryNovelRelationService.findCategoryIdsByNovelId(novelId));
+    }
+
+    public NovelInfoDto viewNovel(NovelViewModelDto novelViewModelDto, Long novelId) {
+        List<CategoryDto> categoryDtos = getCategoryDtoList(novelId);
+        return getNovelInfoDto(
+                novelViewModelDto.getNovelId(),
+                novelViewModelDto.getTitle(),
+                novelViewModelDto.getAuthorName(),
+                novelViewModelDto.getCoverImageUrl(),
+                novelViewModelDto.getDescription(),
+                novelViewModelDto.isDeleted(),
+                novelViewModelDto.getAge(),
+                novelViewModelDto.isPremium(),
+                novelViewModelDto.getUpdateTime(),
+                novelViewModelDto.getPreferenceCount(),
+                novelViewModelDto.getBookMarkCount(),
+                novelViewModelDto.getView(),
+                novelViewModelDto.getEpisodeNumber(),
                 categoryDtos
         );
     }
@@ -269,27 +306,4 @@ public class NovelManagerService {
         );
     }
 
-    public NovelInfoDto viewNovelWithPreference(Long novelId, Long preferenceCount) {
-        NovelDto novelDto = novelService.getNovelDto(novelId);
-        List<Integer> episodeIds = episodeService.findAllByNovelId(novelId)
-                .stream().map(EpisodeDto::getEpisodeId).collect(Collectors.toList());
-        UpdateTimeDto updateTimeDto = updateTimeService.getUpdateTime(novelId);
-        List<CategoryDto> categoryDtos = categoryService.findAllByCategoryIds(categoryNovelRelationService.findCategoryIdsByNovelId(novelId));
-        return getNovelInfoDto(
-                novelDto.getNovelId(),
-                novelDto.getTitle(),
-                novelDto.getAuthorName(),
-                novelDto.getCoverImageUrl(),
-                novelDto.getDescription(),
-                novelDto.isDeleted(),
-                novelDto.getAge(),
-                gradeService.getGrade(novelId).isPremium(),
-                updateTimeDto.getUpdateTime(),
-                preferenceCount,
-                bookMarkService.getTotalBookMarkCount(novelId),
-                viewCountService.getTotalViewCount(novelId, episodeIds),
-                episodeService.getTotalEpisode(novelId),
-                categoryDtos
-        );
-    }
 }
