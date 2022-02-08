1134802f458b995bd2b6d922eaa64e3182fef80e
horobito <odkfu1@gmail.com>
2022-02-08 20:03:57 +0900

    feat : apply [NovelViewManagerService] in [EpisodeManagerService]

--- src/main/java/myproject/demo/manager/episodeManager/service/EpisodeManagerService.java
+++ src/main/java/myproject/demo/manager/episodeManager/service/EpisodeManagerService.java
@@ -3,58 +3,41 @@ package myproject.demo.manager.episodeManager.service;
 import lombok.RequiredArgsConstructor;
 import myproject.demo.Episode.service.EpisodeDto;
 import myproject.demo.Episode.service.EpisodeService;
-import myproject.demo.Novel.service.NovelDto;
-import myproject.demo.Novel.service.NovelService;
 import myproject.demo.Preference.service.PreferenceService;
-import myproject.demo.bookmark.service.BookMarkService;
-import myproject.demo.category.service.CategoryDto;
-import myproject.demo.category.service.CategoryService;
-import myproject.demo.category_novel.service.CategoryNovelRelationService;
-import myproject.demo.grade.service.GradeService;
+import myproject.demo.novelViewModel.service.NovelViewModelService;
 import myproject.demo.updateTime.service.UpdateTimeService;
 import myproject.demo.view.service.ViewService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class EpisodeManagerService {
-
-    private final NovelService novelService;
-
-    private final CategoryService categoryService;
-
-    private final CategoryNovelRelationService categoryNovelRelationService;
-
-    private final GradeService gradeService;
 
     private final EpisodeService episodeService;
 
     private final PreferenceService preferenceService;
 
-
     private final UpdateTimeService updateTimeService;
-
-    private final BookMarkService bookMarkService;
 
     private final ViewService viewCountService;
 
+    private final NovelViewModelService novelViewModelService;
+
     @Transactional
-    public EpisodeInfoDto createEpisode(Long novelId, String title, String content, String authorComment, int age) throws IllegalAccessException {
+    public EpisodeInfoDto createEpisode(Long novelId, String title, String content, String authorComment, int age){
         EpisodeDto episodeDto = episodeService.create(novelId, title, content, authorComment, age);
         preferenceService.createCount(novelId, episodeDto.getEpisodeId());
         viewCountService.createViewInfo(novelId, episodeDto.getEpisodeId());
         updateTimeService.update(novelId);
+        novelViewModelService.increaseEpisodeCount(novelId);
         return getEpisodeInfoDto(
                 novelId, episodeDto
         );
     }
 
     @Transactional
-    public EpisodeInfoDto updateEpisode(Long novelId, int episodeId, String title, String content, String authorComment, int age) throws IllegalAccessException {
+    public EpisodeInfoDto updateEpisode(Long novelId, int episodeId, String title, String content, String authorComment, int age){
         episodeService.update(novelId, episodeId, title, content, authorComment, age);
         updateTimeService.update(novelId);
 
@@ -64,10 +47,11 @@ public class EpisodeManagerService {
     }
 
     @Transactional
-    public EpisodeInfoDto deleteEpisode(Long novelId, int episodeNum) throws IllegalAccessException {
+    public EpisodeInfoDto deleteEpisode(Long novelId, int episodeNum){
         viewCountService.delete(novelId, episodeNum);
         preferenceService.deleteCount(novelId, episodeNum);
         episodeService.delete(novelId, episodeNum);
+        novelViewModelService.decreaseEpisodeCount(novelId);
         return getEpisodeInfoDto(
                 novelId, episodeService.getEpisode(novelId, episodeNum)
         );
@@ -78,7 +62,7 @@ public class EpisodeManagerService {
         episodeService.resurrect(novelId, episodeNum);
         viewCountService.resurrect(novelId, episodeNum);
         preferenceService.resurrect(novelId, episodeNum);
-
+        novelViewModelService.increaseEpisodeCount(novelId);
         return getEpisodeInfoDto(
                 novelId, episodeService.getEpisode(novelId, episodeNum)
         );
