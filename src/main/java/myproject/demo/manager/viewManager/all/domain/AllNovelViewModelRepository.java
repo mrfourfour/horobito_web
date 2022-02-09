ef9f746edce34f6bc92c3b46be1bf2cadc4ff6c2
horobito <odkfu1@gmail.com>
2022-02-09 23:17:54 +0900

    feat: implement queries and methods related with <getAllNovelListByCategory>

--- src/main/java/myproject/demo/manager/viewManager/all/domain/AllNovelViewModelRepository.java
+++ src/main/java/myproject/demo/manager/viewManager/all/domain/AllNovelViewModelRepository.java
@@ -45,13 +45,13 @@ public interface AllNovelViewModelRepository extends JpaRepository<NovelViewMode
 
     @Query(value = "select semi.*\n" +
             "from (\n" +
-            "         select ROW_NUMBER() over (ORDER BY pre.update_time asc ) as updateTimeRaking , pre.*\n" +
-            "         from (select * from novel_view_model nvm where nvm.deleted=false order by nvm.update_time asc ) as `pre`) as semi\n" +
+            "         select ROW_NUMBER() over (ORDER BY pre.update_time desc ) as updateTimeRaking , pre.*\n" +
+            "         from (select * from novel_view_model nvm where nvm.deleted=false order by nvm.update_time desc ) as `pre`) as semi\n" +
             "where semi.updateTimeRaking> case when :cursor>0 then (\n" +
             "    select semi.updateTimeRaking\n" +
             "    from\n" +
-            "        (select ROW_NUMBER() over (ORDER BY pre.update_time asc ) as updateTimeRaking , pre.*\n" +
-            "         from (select * from novel_view_model nvm where nvm.deleted=false order by nvm.update_time asc ) as pre) as semi\n" +
+            "        (select ROW_NUMBER() over (ORDER BY pre.update_time desc ) as updateTimeRaking , pre.*\n" +
+            "         from (select * from novel_view_model nvm where nvm.deleted=false order by nvm.update_time desc ) as pre) as semi\n" +
             "    where semi.novel_id = :cursor)\n" +
             "                            else 0 end\n" +
             "limit :size", nativeQuery = true)
@@ -87,4 +87,95 @@ public interface AllNovelViewModelRepository extends JpaRepository<NovelViewMode
             "                                    else 0 end\n" +
             "limit :size", nativeQuery = true)
     List<NovelViewModel> findAllInBookMarkCountOrder(Long cursor, int size);
+
+    
+    
+    
+    
+    
+    @Query(value = "select semi.*\n" +
+            "from (\n" +
+            "         select ROW_NUMBER() over (ORDER BY pre.view desc ) as viewRaking , pre.*\n" +
+            "         from (select nvm.* from novel_view_model nvm left outer join  category_novel_relation cnr on cnr.deleted=false and cnr.category_id=:categoryId\n" +
+            "                where cnr.novel_id = nvm.novel_id and nvm.deleted=false order by nvm.view desc ) as `pre`) as semi\n" +
+            "where semi.viewRaking> case when :cursor>0 then (\n" +
+            "    select semi.viewRaking\n" +
+            "    from\n" +
+            "        (select ROW_NUMBER() over (ORDER BY pre.view desc ) as viewRaking , pre.*\n" +
+            "         from (select nvm.* from novel_view_model nvm left outer join category_novel_relation cnr on cnr.deleted=false and cnr.category_id=:categoryId\n" +
+            "               where cnr.novel_id = nvm.novel_id and nvm.deleted=false order by nvm.view desc) as pre) as semi\n" +
+            "    where semi.novel_id = :cursor)\n" +
+            "                            else 0 end\n" +
+            "limit :size;", nativeQuery = true)
+    List<NovelViewModel> findAllInViewOrderWithCategory(Long categoryId, Long cursor, int size);
+
+    @Query(value = "select semi.*\n" +
+            "from (\n" +
+            "         select ROW_NUMBER() over (ORDER BY pre.total_count desc ) as preferenceRaking , pre.*\n" +
+            "         from (select nvm.*, tpc.total_count from novel_view_model nvm\n" +
+            "             left join category_novel_relation cnr left join  total_preference_count tpc\n" +
+            "                 on tpc.deleted=false on cnr.category_id =:categoryId and nvm.deleted=false\n" +
+            "                  where tpc.novel_id=nvm.novel_id and cnr.novel_id=nvm.novel_id order by tpc.total_count desc ) as `pre`) as semi\n" +
+            "where semi.preferenceRaking> case when :cursor>0 then (\n" +
+            "    select semi.preferenceRaking\n" +
+            "    from\n" +
+            "        (select ROW_NUMBER() over (ORDER BY pre.total_count desc ) as preferenceRaking , pre.*\n" +
+            "         from (select nvm.*, tpc.total_count from novel_view_model nvm\n" +
+            "             left join category_novel_relation cnr left join  total_preference_count tpc\n" +
+            "                 on tpc.deleted=false on cnr.category_id =:categoryId and nvm.deleted=false\n" +
+            "                 where tpc.novel_id=nvm.novel_id and cnr.novel_id=nvm.novel_id order by tpc.total_count desc ) as pre) as semi\n" +
+            "    where semi.novel_id = :cursor)\n" +
+            "                            else 0 end\n" +
+            "limit :size;", nativeQuery = true)
+    List<NovelViewModel> findAllInPreferenceOrderWithCategory(Long categoryId, Long cursor, int size);
+
+    @Query(value = "select semi.*\n" +
+            "from (\n" +
+            "         select ROW_NUMBER() over (ORDER BY pre.update_time desc  ) as updateTimeRaking , pre.*\n" +
+            "         from (select nvm.* from novel_view_model nvm left outer join  category_novel_relation cnr on cnr.deleted=false and cnr.category_id=:categoryId\n" +
+            "               where cnr.novel_id = nvm.novel_id and nvm.deleted=false order by nvm.update_time desc  ) as `pre`) as semi\n" +
+            "where semi.updateTimeRaking> case when :cursor>0 then (\n" +
+            "    select semi.updateTimeRaking\n" +
+            "    from\n" +
+            "        (select ROW_NUMBER() over (ORDER BY pre.update_time desc ) as updateTimeRaking , pre.*\n" +
+            "         from (select nvm.* from novel_view_model nvm left outer join category_novel_relation cnr on cnr.deleted=false and cnr.category_id=:categoryId\n" +
+            "               where cnr.novel_id = nvm.novel_id and nvm.deleted=false order by nvm.update_time desc ) as pre) as semi\n" +
+            "    where semi.novel_id = :cursor)\n" +
+            "                            else 0 end\n" +
+            "limit :size;", nativeQuery = true)
+    List<NovelViewModel> findAllInDateOrderWithCategory(Long categoryId, Long cursor, int size);
+
+
+    @Query(value = "select semi.*\n" +
+            "from (\n" +
+            "         select ROW_NUMBER() over (ORDER BY pre.episode_number desc ) as episodeCountRaking , pre.*\n" +
+            "         from (select nvm.* from novel_view_model nvm left outer join  category_novel_relation cnr on cnr.deleted=false and cnr.category_id=:categoryId\n" +
+            "               where cnr.novel_id = nvm.novel_id and nvm.deleted=false order by nvm.episode_number desc ) as `pre`) as semi\n" +
+            "where semi.episodeCountRaking> case when :cursor>0 then (\n" +
+            "    select semi.episodeCountRaking\n" +
+            "    from\n" +
+            "        (select ROW_NUMBER() over (ORDER BY pre.episode_number desc  ) as episodeCountRaking , pre.*\n" +
+            "         from (select nvm.* from novel_view_model nvm left outer join category_novel_relation cnr on cnr.deleted=false and cnr.category_id=:categoryId\n" +
+            "               where cnr.novel_id = nvm.novel_id and nvm.deleted=false order by nvm.episode_number desc ) as pre) as semi\n" +
+            "    where semi.novel_id = :cursor)\n" +
+            "                                  else 0 end\n" +
+            "limit :size;", nativeQuery = true)
+    List<NovelViewModel> findAllInEpisodeCountOrderWithCategory(Long categoryId, Long cursor, int size);
+
+
+    @Query(value = "select semi.*\n" +
+            "from (\n" +
+            "         select ROW_NUMBER() over (ORDER BY pre.book_mark_count desc ) as bookmarkCountRanking , pre.*\n" +
+            "         from (select nvm.* from novel_view_model nvm left outer join  category_novel_relation cnr on cnr.deleted=false and cnr.category_id=:categoryId\n" +
+            "               where cnr.novel_id = nvm.novel_id and nvm.deleted=false order by nvm.book_mark_count desc ) as `pre`) as semi\n" +
+            "where semi.bookmarkCountRanking> case when :cursor>0 then (\n" +
+            "    select semi.bookmarkCountRanking\n" +
+            "    from\n" +
+            "        (select ROW_NUMBER() over (ORDER BY pre.book_mark_count desc  ) as bookmarkCountRanking , pre.*\n" +
+            "         from (select nvm.* from novel_view_model nvm left outer join category_novel_relation cnr on cnr.deleted=false and cnr.category_id=:categoryId\n" +
+            "               where cnr.novel_id = nvm.novel_id and nvm.deleted=false order by nvm.book_mark_count desc ) as pre) as semi\n" +
+            "    where semi.novel_id = :cursor)\n" +
+            "                                    else 0 end\n" +
+            "limit :size;", nativeQuery = true)
+    List<NovelViewModel> findAllInBookMarkCountOrderWithCategory(Long categoryId, Long cursor, int size);
 }
