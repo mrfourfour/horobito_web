98e751329b895558762c0ed9fa3dc7d3461ac7e1
horobito <odkfu1@gmail.com>
2022-02-09 22:06:28 +0900

    feat: create [AllNovelViewManagerService] class and  implementing <getAllNovelList()> method

---
+++ src/main/java/myproject/demo/manager/viewManager/all/service/AllNovelViewManagerService.java
@@ -0,0 +1,51 @@
+package myproject.demo.manager.viewManager.all.service;
+
+
+import lombok.RequiredArgsConstructor;
+import myproject.demo.category.service.CategoryService;
+import myproject.demo.manager.novelManager.service.NovelInfoDto;
+import myproject.demo.manager.novelManager.service.NovelManagerService;
+import myproject.demo.manager.viewManager.all.domain.AllNovelViewModelRepository;
+import myproject.demo.novelViewModel.domain.NovelViewModel;
+import myproject.demo.novelViewModel.service.NovelViewModelService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AllNovelViewManagerService {
+
+    private final NovelViewModelService novelViewModelService;
+    private final NovelManagerService novelManagerService;
+    private final AllNovelViewModelRepository allNovelViewModelRepository;
+    private final CategoryService categoryService;
+
+    public List<NovelInfoDto> getAllNovelList(String sort, Long cursor, int size) {
+        if (sort.equals("view")){
+            return getNovelInfoList(allNovelViewModelRepository.findAllInViewOrder(cursor, size));
+        }else if(sort.equals("preference")){
+            return getNovelInfoList(allNovelViewModelRepository.findAllInPreferenceOrder(cursor, size));
+        }else if(sort.equals("date")){
+            return getNovelInfoList(allNovelViewModelRepository.findAllInDateOrder(cursor, size));
+        }else if (sort.equals("episode")){
+            return getNovelInfoList(allNovelViewModelRepository.findAllInEpisodeCountOrder(cursor, size));
+        }else if (sort.equals("book-mark")){
+            return getNovelInfoList(allNovelViewModelRepository.findAllInBookMarkCountOrder(cursor, size));
+        }else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+    public void getAllNovelListByCategory(String categoryName, String sort, Long cursor, int size) {
+        Long categoryId = categoryService.getCategoryIdByName(categoryName);
+    }
+
+
+    public List<NovelInfoDto> getNovelInfoList(List<NovelViewModel> novelViewModelList){
+        return novelViewModelService.getDtoList(novelViewModelList)
+                .stream().map(it->novelManagerService.viewNovel(it, it.getNovelId())).collect(Collectors.toList());
+    }
+}
