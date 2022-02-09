8eb42c98ec3ccef6aa78b030783f3b295b27fcb9
horobito <odkfu1@gmail.com>
2022-02-09 22:08:12 +0900

    feat: create [AllNovelViewManagerRepository] interface and  implementing <findAllInViewOrder>, <findAllInPreferenceOrder>, <findAllInDateOrder>,  <findAllInEpisodeCountOrder>,  <findAllInBookMarkCountOrder> method
     and check their function operation

---
+++ src/main/java/myproject/demo/manager/viewManager/all/domain/AllNovelViewModelRepository.java
@@ -0,0 +1,90 @@
+package myproject.demo.manager.viewManager.all.domain;
+
+import myproject.demo.novelViewModel.domain.NovelViewModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AllNovelViewModelRepository extends JpaRepository<NovelViewModel, Long> {
+
+
+    @Query(value = "select semi.*\n" +
+            "from (\n" +
+            "        select ROW_NUMBER() over (ORDER BY pre.view desc ) as viewRaking , pre.*\n" +
+            "        from (select * from novel_view_model nvm where nvm.deleted=false order by nvm.view desc ) as `pre`) as semi\n" +
+            "where semi.viewRaking> case when :cursor>0 then (\n" +
+            "                                                select semi.viewRaking\n" +
+            "                                                from\n" +
+            "                                                (select ROW_NUMBER() over (ORDER BY pre.view desc ) as viewRaking , pre.*\n" +
+            "                                                from (select * from novel_view_model nvm where nvm.deleted=false order by nvm.view desc ) as pre) as semi\n" +
+            "                                                where semi.novel_id = :cursor)\n" +
+            "                                                else 0 end\n" +
+            "limit :size;", nativeQuery = true)
+    List<NovelViewModel> findAllInViewOrder(Long cursor, int size);
+
+
+
+
+    @Query(value = "select semi.*\n" +
+            "from (\n" +
+            "         select ROW_NUMBER() over (ORDER BY pre.total_count desc ) as viewRaking , pre.*\n" +
+            "         from (select nvm.*, tpc.total_count from novel_view_model nvm left outer join  total_preference_count tpc on tpc.deleted=false and nvm.deleted=false where tpc.novel_id=nvm.novel_id order by tpc.total_count desc ) as `pre`) as semi\n" +
+            "where semi.viewRaking> case when :cursor>0 then (\n" +
+            "    select semi.viewRaking\n" +
+            "    from\n" +
+            "        (select ROW_NUMBER() over (ORDER BY pre.total_count desc ) as viewRaking , pre.*\n" +
+            "         from (select nvm.*, tpc.total_count from novel_view_model nvm left outer join  total_preference_count tpc on tpc.deleted=false and nvm.deleted=false where tpc.novel_id=nvm.novel_id order by tpc.total_count desc ) as pre) as semi\n" +
+            "    where semi.novel_id = :cursor)\n" +
+            "                            else 0 end\n" +
+            "limit :size;", nativeQuery = true)
+    List<NovelViewModel> findAllInPreferenceOrder(Long cursor, int size);
+
+
+
+
+    @Query(value = "select semi.*\n" +
+            "from (\n" +
+            "         select ROW_NUMBER() over (ORDER BY pre.update_time asc ) as updateTimeRaking , pre.*\n" +
+            "         from (select * from novel_view_model nvm where nvm.deleted=false order by nvm.update_time asc ) as `pre`) as semi\n" +
+            "where semi.updateTimeRaking> case when :cursor>0 then (\n" +
+            "    select semi.updateTimeRaking\n" +
+            "    from\n" +
+            "        (select ROW_NUMBER() over (ORDER BY pre.update_time asc ) as updateTimeRaking , pre.*\n" +
+            "         from (select * from novel_view_model nvm where nvm.deleted=false order by nvm.update_time asc ) as pre) as semi\n" +
+            "    where semi.novel_id = :cursor)\n" +
+            "                            else 0 end\n" +
+            "limit :size", nativeQuery = true)
+    List<NovelViewModel> findAllInDateOrder(Long cursor, int size);
+
+
+
+    @Query(value = "select semi.*\n" +
+            "from (\n" +
+            "         select ROW_NUMBER() over (ORDER BY pre.episode_number desc ) as episodeContRanking , pre.*\n" +
+            "         from (select * from novel_view_model nvm where nvm.deleted=false order by nvm.episode_number desc ) as `pre`) as semi\n" +
+            "where semi.episodeContRanking> case when :cursor>0 then (\n" +
+            "    select semi.episodeContRanking\n" +
+            "    from\n" +
+            "        (select ROW_NUMBER() over (ORDER BY pre.episode_number desc ) as episodeContRanking , pre.*\n" +
+            "         from (select * from novel_view_model nvm where nvm.deleted=false order by nvm.episode_number desc ) as pre) as semi\n" +
+            "    where semi.novel_id = :cursor)\n" +
+            "                                  else 0 end\n" +
+            "limit :size", nativeQuery = true)
+    List<NovelViewModel> findAllInEpisodeCountOrder(Long cursor, int size);
+
+
+    @Query(value = "select semi.*\n" +
+            "from (\n" +
+            "         select ROW_NUMBER() over (ORDER BY pre.book_mark_count desc ) as bookmarkCountRanking , pre.*\n" +
+            "         from (select * from novel_view_model nvm where nvm.deleted=false order by nvm.book_mark_count desc ) as `pre`) as semi\n" +
+            "where semi.bookmarkCountRanking> case when :cursor>0 then (\n" +
+            "    select semi.bookmarkCountRanking\n" +
+            "    from\n" +
+            "        (select ROW_NUMBER() over (ORDER BY pre.book_mark_count desc ) as bookmarkCountRanking , pre.*\n" +
+            "         from (select * from novel_view_model nvm where nvm.deleted=false order by nvm.book_mark_count desc ) as pre) as semi\n" +
+            "    where semi.novel_id = :cursor)\n" +
+            "                                    else 0 end\n" +
+            "limit :size", nativeQuery = true)
+    List<NovelViewModel> findAllInBookMarkCountOrder(Long cursor, int size);
+}
