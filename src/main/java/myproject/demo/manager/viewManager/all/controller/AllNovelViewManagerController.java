a05357027f14d0186e9380f42ea9cbba6e39887c
horobito <odkfu1@gmail.com>
2022-02-10 19:06:46 +0900

    feat: change endpoint  and apply  <getAdultNovelList>,  <getAdultNovelListByCategory>,  <getNonAdultNovelList> and  <getNonAdultNovelListByCategory> methods

--- src/main/java/myproject/demo/manager/viewManager/all/controller/AllNovelViewManagerController.java
+++ src/main/java/myproject/demo/manager/viewManager/all/controller/AllNovelViewManagerController.java
@@ -1,16 +1,18 @@
 package myproject.demo.manager.viewManager.all.controller;
 
 import lombok.RequiredArgsConstructor;
+import myproject.demo.manager.novelManager.service.NovelInfoDto;
 import myproject.demo.manager.viewManager.all.service.AllNovelViewManagerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
-@Controller
-@RequestMapping("/novels/free")
+@RestController
+@RequestMapping("/novels")
 @RequiredArgsConstructor
 public class AllNovelViewManagerController {
 
@@ -19,42 +21,50 @@ public class AllNovelViewManagerController {
 
 
     @GetMapping("/all")
-    public void getAll(
+    public ResponseEntity<List<NovelInfoDto> >getAll(
             @PathVariable String categoryName,
             @RequestParam(value = "sort") String sort,
             @RequestParam(value = "cursor") Long cursor,
             @RequestParam(value = "size") int size
     ) {
         if (categoryName.equals("all")) {
-            allNovelViewManagerService.getAllNovelList(sort, cursor, size);
+            return ResponseEntity.ok(allNovelViewManagerService.getAllNovelList(sort, cursor, size));
         } else {
-            allNovelViewManagerService.getAllNovelListByCategory(categoryName, sort, cursor, size);
+            return ResponseEntity.ok(allNovelViewManagerService.getAllNovelListByCategory(categoryName, sort, cursor, size));
         }
     }
 
     @GetMapping("/adult")
-    public void getAdult(
+    public ResponseEntity<List<NovelInfoDto>> getAdult(
             @PathVariable String categoryName,
             @RequestParam(value = "sort") String sort,
             @RequestParam(value = "cursor") Long cursor,
             @RequestParam(value = "size") int size
     ) {
         if (categoryName.equals("all")) {
-
+            return ResponseEntity.ok(allNovelViewManagerService.getAdultNovelList(sort, cursor, size));
         } else {
+            return ResponseEntity.ok(allNovelViewManagerService.getAdultNovelListByCategory(categoryName, sort, cursor, size));
 
         }
     }
 
     @GetMapping("/non-adult")
-    public void getNonAdult(
+    public ResponseEntity<List<NovelInfoDto>> getNonAdult(
             @PathVariable String categoryName,
             @RequestParam(value = "sort") String sort,
             @RequestParam(value = "cursor") Long cursor,
             @RequestParam(value = "size") int size
     ) {
         if (categoryName.equals("all")) {
+            return ResponseEntity.ok(allNovelViewManagerService.getNonAdultNovelList(sort, cursor, size));
         } else {
+            return ResponseEntity.ok(allNovelViewManagerService.getNonAdultNovelListByCategory(categoryName, sort, cursor, size));
         }
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Void> handleIllegalArgumentException(){
+        return ResponseEntity.badRequest().build();
     }
 }
