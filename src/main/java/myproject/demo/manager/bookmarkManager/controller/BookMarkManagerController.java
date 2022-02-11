89e376a2cba50e17931b9145c52dabca894a9003
horobito <odkfu1@gmail.com>
2022-02-11 17:13:55 +0900

    refactor : change endpoint

--- src/main/java/myproject/demo/manager/bookmarkManager/controller/BookMarkManagerController.java
+++ src/main/java/myproject/demo/manager/bookmarkManager/controller/BookMarkManagerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("book-mark")
+@RequestMapping("/book-mark")
 public class BookMarkManagerController {
 
     private final BookMarkManagerService bookMarkManagerService;
